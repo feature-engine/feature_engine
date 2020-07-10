woe formula fix, new discretizer, inverse_transform and na allowed in outlier classes
* starting new develop branch

* remove pypirc update circleci conf (#40)

* updated pypirc and circleconf

* adjust ci config

* remove .pypirc

* pip permissions

* fix pip

* use venv

* add filter on master to cci config

* add filter on master to cci config 2

Co-authored-by: Soledad Galli <solegalli1@gmail.com>
Co-authored-by: ChristopherGS <christopher.samiullah@gmail.com>

* increased version

* added func: discretisers return intervals (#42)

* Wrapper (#43)

* add discretiser boundaries to changelog

* created sklearn wrapper

Co-authored-by: Soledad Galli <solegalli1@gmail.com>

* added docs for contributing

* added docs for contributing (#45)

* update changelog with discretiser, contribute and sklearn wrapper (#44)

* add discretiser boundaries to changelog

* added sklearn wrapper to changelog

* updated changelog with contribute page

Co-authored-by: Soledad Galli <solegalli1@gmail.com>

* fixed format in rst

* Update Wrapper.rst

* Update changelog.rst

* Update README.md (#49)

* fixed lack of indentation, outlierTrimmer.rst

* removed capping on version for req files

* fixed quickstart code indentation

* fixed winzorizer code indentation

* fixed trimmer code indentation in docs

* fixed code indentation sklearwrapper

* added patch increase to version

* outlier transf can now take na

* bug fix woe

* aded user input discretiser

* added inverse_transform to encoders

* inverse transform and bug fixes

* final fixes and docs

* style fix base_transformers

feature_engine/categorical_encoders.py
@@ -118,6 +118,12 @@ class CountFrequencyCategoricalEncoder(BaseCategoricalTransformer):
 
     transform.__doc__ = BaseCategoricalTransformer.transform.__doc__
 
+    def inverse_transform(self, X):
+        X = super().inverse_transform(X)
+        return X
+
+    inverse_transform.__doc__ = BaseCategoricalTransformer.inverse_transform.__doc__
+
 
 class OrdinalCategoricalEncoder(BaseCategoricalTransformer):
     """ 
@@ -232,6 +238,12 @@ class OrdinalCategoricalEncoder(BaseCategoricalTransformer):
 
     transform.__doc__ = BaseCategoricalTransformer.transform.__doc__
 
+    def inverse_transform(self, X):
+        X = super().inverse_transform(X)
+        return X
+
+    inverse_transform.__doc__ = BaseCategoricalTransformer.inverse_transform.__doc__
+
 
 class MeanCategoricalEncoder(BaseCategoricalTransformer):
     """ 
@@ -317,27 +329,38 @@ class MeanCategoricalEncoder(BaseCategoricalTransformer):
 
     transform.__doc__ = BaseCategoricalTransformer.transform.__doc__
 
+    def inverse_transform(self, X):
+        X = super().inverse_transform(X)
+        return X
+
+    inverse_transform.__doc__ = BaseCategoricalTransformer.inverse_transform.__doc__
+
 
 class WoERatioCategoricalEncoder(BaseCategoricalTransformer):
     """ 
     The WoERatioCategoricalEncoder() replaces categories by the weight of evidence
     or by the ratio between the probability of the target = 1 and the probability
     of the  target = 0.
-    
-    The weight of evidence is given by: np.log( p(1) / p(0) )
+
+    The weight of evidence is given by: np.log(P(X=xj|Y = 1)/P(X=xj|Y=0))
     
     The target probability ratio is given by: p(1) / p(0)
+
+    And the log of the target probability ratio is: np.log( p(1) / p(0) )
     
     Note: This categorical encoding is exclusive for binary classification.
     
     For example in the variable colour, if the mean of the target = 1 for blue
     is 0.8 and the mean of the target = 0  is 0.2, blue will be replaced by:
-    np.log(0.8/0.2) = 1.386 if woe is selected. Alternatively, blue will be 
-    replaced by 0.8 / 0.2 = 4.
+    np.log(0.8/0.2) = 1.386 if log_ratio is selected. Alternatively, blue will be
+    replaced by 0.8 / 0.2 = 4 if ratio is selected.
+
+    For details on the calculation of the weight of evidence visit:
+    https://www.listendata.com/2015/03/weight-of-evidence-woe-and-information.html
     
     Note: the division by 0 is not defined and the log(0) is not defined.
     Thus, if p(0) = 0 for the ratio encoder, or either p(0) = 0 or p(1) = 0 for
-    woe, in any of the variables, the encoder will return an error.
+    woe or log_ratio, in any of the variables, the encoder will return an error.
        
     The encoder will encode only categorical variables (type 'object'). A list
     of variables can be passed as an argument. If no variables are passed as 
@@ -365,8 +388,8 @@ class WoERatioCategoricalEncoder(BaseCategoricalTransformer):
 
     def __init__(self, encoding_method='woe', variables=None):
 
-        if encoding_method not in ['woe', 'ratio']:
-            raise ValueError("encoding_method takes only values 'woe' and 'ratio'")
+        if encoding_method not in ['woe', 'ratio', 'log_ratio']:
+            raise ValueError("encoding_method takes only values 'woe', 'ratio' and 'log_ratio'")
 
         self.encoding_method = encoding_method
         self.variables = _define_variables(variables)
@@ -414,24 +437,44 @@ class WoERatioCategoricalEncoder(BaseCategoricalTransformer):
 
         self.encoder_dict_ = {}
 
-        for var in self.variables:
-            t = temp.groupby(var)['target'].mean()
-            t = pd.concat([t, 1 - t], axis=1)
-            t.columns = ['p1', 'p0']
+        if self.encoding_method == 'woe':
+            total_pos = temp['target'].sum()
+            total_neg = len(temp) - total_pos
+            temp['non_target'] = np.where(temp['target'] == 1, 0, 1)
 
-            if self.encoding_method == 'woe':
-                if not t.loc[t['p0'] == 0, :].empty or not t.loc[t['p1'] == 0, :].empty:
-                    raise ValueError(
-                        "p(0) or p(1) for a category in variable {} is zero, log of zero is not defined".format(var))
-                else:
-                    self.encoder_dict_[var] = (np.log(t.p1 / t.p0)).to_dict()
+            for var in self.variables:
+                pos = temp.groupby([var])['target'].sum() / total_pos
+                neg = temp.groupby([var])['non_target'].sum() / total_neg
 
-            elif self.encoding_method == 'ratio':
-                if not t.loc[t['p0'] == 0, :].empty:
+                t = pd.concat([pos, neg], axis=1)
+                t['woe'] = np.log(t['target'] / t['non_target'])
+
+                if not t.loc[t['target'] == 0, :].empty or not t.loc[t['non_target'] == 0, :].empty:
                     raise ValueError(
-                        "p(0) for a category in variable {} is zero, division by 0 is not defined".format(var))
-                else:
-                    self.encoder_dict_[var] = (t.p1 / t.p0).to_dict()
+                        "The proportion of 1 of the classes for a category in variable {} is zero, and log of zero is "
+                        "not defined".format(var))
+
+                self.encoder_dict_[var] = t['woe'].to_dict()
+
+        else:
+            for var in self.variables:
+                t = temp.groupby(var)['target'].mean()
+                t = pd.concat([t, 1 - t], axis=1)
+                t.columns = ['p1', 'p0']
+
+                if self.encoding_method == 'log_ratio':
+                    if not t.loc[t['p0'] == 0, :].empty or not t.loc[t['p1'] == 0, :].empty:
+                        raise ValueError(
+                            "p(0) or p(1) for a category in variable {} is zero, log of zero is not defined".format(var))
+                    else:
+                        self.encoder_dict_[var] = (np.log(t.p1 / t.p0)).to_dict()
+
+                elif self.encoding_method == 'ratio':
+                    if not t.loc[t['p0'] == 0, :].empty:
+                        raise ValueError(
+                            "p(0) for a category in variable {} is zero, division by 0 is not defined".format(var))
+                    else:
+                        self.encoder_dict_[var] = (t.p1 / t.p0).to_dict()
 
         self.encoder_dict_ = _check_encoding_dictionary(self.encoder_dict_)
 
@@ -445,6 +488,12 @@ class WoERatioCategoricalEncoder(BaseCategoricalTransformer):
         return X
 
     transform.__doc__ = BaseCategoricalTransformer.transform.__doc__
+
+    def inverse_transform(self, X):
+        X = super().inverse_transform(X)
+        return X
+
+    inverse_transform.__doc__ = BaseCategoricalTransformer.inverse_transform.__doc__
 
 
 class OneHotCategoricalEncoder(BaseEstimator, TransformerMixin):

feature_engine/outlier_removers.py
@@ -1,14 +1,23 @@
-# Authors: Soledad Galli <solegalli1@gmail.com>
+# Authors: Soledad Galli <solegalli@protonmail.com>
 # License: BSD 3 clause
 
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
-from feature_engine.dataframe_checks import _is_dataframe, _check_input_matches_training_df, _check_contains_na
-from feature_engine.variable_manipulation import _define_variables
-from feature_engine.base_transformers import BaseNumericalTransformer
+
+from feature_engine.variable_manipulation import (
+    _define_variables,
+    _find_numerical_variables
+)
+
+from feature_engine.dataframe_checks import (
+    _is_dataframe,
+    _check_input_matches_training_df,
+    _check_contains_na,
+)
 
 
-class Winsorizer(BaseNumericalTransformer):
+class Winsorizer(BaseEstimator, TransformerMixin):
     """
     The Winsorizer() caps maximum and / or minimum values of a variable.
     
@@ -81,13 +90,16 @@ class Winsorizer(BaseNumericalTransformer):
 
         If distribution='quantile', then 'fold' indicates the percentile. So if
         fold=0.05, the limits will be the 95th and 5th percentiles.
-        
+        Note: Outliers will be removed up to a maximum of the 20th percentiles on both
+        sides. Thus, when distribution='quantile', then 'fold' takes values between 0
+        and 0.20.
+
     variables : list, default=None
         The list of variables for which the outliers will be capped. If None, 
         the transformer will find and select all numerical variables.
     """
 
-    def __init__(self, distribution='gaussian', tail='right', fold=3, variables=None):
+    def __init__(self, distribution='gaussian', tail='right', fold=3, variables=None, missing_values='raise'):
 
         if distribution not in ['gaussian', 'skewed', 'quantiles']:
             raise ValueError("distribution takes only values 'gaussian', 'skewed' or 'quantiles'")
@@ -98,10 +110,17 @@ class Winsorizer(BaseNumericalTransformer):
         if fold <= 0:
             raise ValueError("fold takes only positive numbers")
 
+        if distribution == 'quantiles' and fold > 0.2:
+            raise ValueError("with distribution='quantiles', fold takes values between 0 and 0.20 only.")
+
+        if missing_values not in ['raise', 'ignore']:
+            raise ValueError("missing_values takes only values 'raise' or 'ignore'")
+
         self.distribution = distribution
         self.tail = tail
         self.fold = fold
         self.variables = _define_variables(variables)
+        self.missing_values = missing_values
 
     def fit(self, X, y=None):
         """ 
@@ -127,8 +146,16 @@ class Winsorizer(BaseNumericalTransformer):
             The dictionary containing the minimum values at which variables
             will be capped.
         """
+
         # check input dataframe
-        X = super().fit(X, y)
+        X = _is_dataframe(X)
+
+        # find or check for numerical variables
+        self.variables = _find_numerical_variables(X, self.variables)
+
+        if self.missing_values == 'raise':
+            # check if dataset contains na
+            _check_contains_na(X, self.variables)
 
         self.right_tail_caps_ = {}
         self.left_tail_caps_ = {}
@@ -143,7 +170,7 @@ class Winsorizer(BaseNumericalTransformer):
                 self.right_tail_caps_ = (X[self.variables].quantile(0.75) + (IQR * self.fold)).to_dict()
 
             elif self.distribution == 'quantiles':
-                self.right_tail_caps_ = X[self.variables].quantile(1-self.fold).to_dict()
+                self.right_tail_caps_ = X[self.variables].quantile(1 - self.fold).to_dict()
 
         if self.tail in ['left', 'both']:
             if self.distribution == 'gaussian':
@@ -178,7 +205,18 @@ class Winsorizer(BaseNumericalTransformer):
         """
 
         # check input dataframe an if class was fitted
-        X = super().transform(X)
+        check_is_fitted(self)
+
+        # check that input is a dataframe
+        X = _is_dataframe(X)
+
+        if self.missing_values == 'raise':
+            # check if dataset contains na
+            _check_contains_na(X, self.variables)
+
+        # Check that the dataframe contains the same number of columns
+        # than the dataframe used to fit the imputer.
+        _check_input_matches_training_df(X, self.input_shape_[1])
 
         for feature in self.right_tail_caps_.keys():
             X[feature] = np.where(X[feature] > self.right_tail_caps_[feature], self.right_tail_caps_[feature],
@@ -190,7 +228,7 @@ class Winsorizer(BaseNumericalTransformer):
         return X
 
 
-class ArbitraryOutlierCapper(BaseNumericalTransformer):
+class ArbitraryOutlierCapper(BaseEstimator, TransformerMixin):
     """ 
     The ArbitraryOutlierCapper() caps the maximum or minimum values of a variable
     by an arbitrary value indicated by the user.
@@ -211,7 +249,7 @@ class ArbitraryOutlierCapper(BaseNumericalTransformer):
         
     """
 
-    def __init__(self, max_capping_dict=None, min_capping_dict=None):
+    def __init__(self, max_capping_dict=None, min_capping_dict=None, missing_values='raise'):
 
         if not max_capping_dict and not min_capping_dict:
             raise ValueError("Please provide at least 1 dictionary with the capping values per variable")
@@ -235,11 +273,16 @@ class ArbitraryOutlierCapper(BaseNumericalTransformer):
             tmp.update(max_capping_dict)
             self.variables = [x for x in tmp.keys()]
 
+        if missing_values not in ['raise', 'ignore']:
+            raise ValueError("missing_values takes only values 'raise' or 'ignore'")
+
+        self.missing_values = missing_values
+
     def fit(self, X, y=None):
         """
-        
         Parameters
         ----------
+
         X : pandas dataframe of shape = [n_samples, n_features]
             The training input samples.
 
@@ -257,7 +300,14 @@ class ArbitraryOutlierCapper(BaseNumericalTransformer):
             The dictionary containing the minimum values at which variables
             will be capped.
         """
-        super().fit(X, y)
+        X = _is_dataframe(X)
+
+        if self.missing_values == 'raise':
+            # check if dataset contains na
+            _check_contains_na(X, self.variables)
+
+        # find or check for numerical variables
+        self.variables = _find_numerical_variables(X, self.variables)
 
         if self.max_capping_dict is not None:
             self.right_tail_caps_ = self.max_capping_dict
@@ -290,9 +340,21 @@ class ArbitraryOutlierCapper(BaseNumericalTransformer):
             The dataframe with the capped variables.
         """
 
-        # check input dataframe an if class was fitted
-        X  = super().transform(X)
+        # check if class was fitted
+        check_is_fitted(self)
 
+        # check that input is a dataframe
+        X = _is_dataframe(X)
+
+        if self.missing_values == 'raise':
+            # check if dataset contains na
+            _check_contains_na(X, self.variables)
+
+        # Check that the dataframe contains the same number of columns
+        # than the dataframe used to fit the imputer.
+        _check_input_matches_training_df(X, self.input_shape_[1])
+
+        # replace outliers
         for feature in self.right_tail_caps_.keys():
             X[feature] = np.where(X[feature] > self.right_tail_caps_[feature], self.right_tail_caps_[feature],
                                   X[feature])
@@ -377,6 +439,9 @@ class OutlierTrimmer(Winsorizer):
 
         If distribution='quantile', then 'fold' indicates the percentile. So if
         fold=0.05, the limits will be the 95th and 5th percentiles.
+        Note: Outliers will be removed up to a maximum of the 20th percentiles on both
+        sides. Thus, when distribution='quantile', then 'fold' takes values between 0
+        and 0.20.
 
     variables : list, default=None
         The list of variables for which the outliers will be capped. If None,
@@ -405,8 +470,9 @@ class OutlierTrimmer(Winsorizer):
         # check that input is a dataframe
         X = _is_dataframe(X)
 
-        # check if dataset contains na
-        _check_contains_na(X, self.variables)
+        if self.missing_values == 'raise':
+            # check if dataset contains na
+            _check_contains_na(X, self.variables)
 
         # Check that the dataframe contains the same number of columns than the dataframe
         # used to fit the imputer.

tests/test_outlier_handler.py
@@ -114,6 +114,26 @@ def test_Windsorizer(dataframe_normal_dist, dataframe_na, dataframe_vartypes):
     assert X['var'].max() <= 0.11823196128033647
     assert dataframe_normal_dist['var'].max() > 0.11823196128033647
 
+    # test case 7: dataset contains na and transformer is asked to ignore them
+    transformer = Winsorizer(distribution='gaussian', tail='right', fold=1,
+                             variables=['Age', 'Marks'],
+                             missing_values='ignore')
+    X = transformer.fit_transform(dataframe_na)
+
+    df_transf = dataframe_na.copy()
+    df_transf['Age'] = np.where(df_transf['Age'] > 38.79255087111844, 38.79255087111844, df_transf['Age'])
+    df_transf['Marks'] = np.where(df_transf['Marks'] > 0.8970309389976613, 0.8970309389976613, df_transf['Marks'])
+
+    # fit params
+    assert transformer.right_tail_caps_ == {'Age': 38.79255087111844, 'Marks': 0.8970309389976613}
+    assert transformer.left_tail_caps_ == {}
+    assert transformer.input_shape_ == (8, 6)
+    # transform params
+    pd.testing.assert_frame_equal(X, df_transf)
+    assert X['Age'].max() <= 38.79255087111844
+    assert dataframe_na['Age'].max() > 38.79255087111844
+
+    # test error raises
     with pytest.raises(ValueError):
         Winsorizer(distribution='other')
 
@@ -121,14 +141,20 @@ def test_Windsorizer(dataframe_normal_dist, dataframe_na, dataframe_vartypes):
         Winsorizer(tail='other')
 
     with pytest.raises(ValueError):
+        Winsorizer(missing_values='other')
+
+    with pytest.raises(ValueError):
         Winsorizer(fold=-1)
 
-    # test case 7: when dataset contains na, fit method
+    with pytest.raises(ValueError):
+        Winsorizer(distribution='quantiles', fold=0.3)
+
+    # test case 8: when dataset contains na, fit method
     with pytest.raises(ValueError):
         transformer = Winsorizer()
         transformer.fit(dataframe_na)
 
-    # test case 8: when dataset contains na, transform method
+    # test case 9: when dataset contains na, transform method
     with pytest.raises(ValueError):
         transformer = Winsorizer()
         transformer.fit(dataframe_vartypes)
@@ -197,6 +223,23 @@ def test_ArbitraryOutlierCapper(dataframe_normal_dist, dataframe_na, dataframe_v
     assert X['var'].min() >= -0.17486039103044
     assert dataframe_normal_dist['var'].min() < -0.17486039103044
 
+    # test case 4: dataset contains na and transformer is asked to ignore them
+    transformer = ArbitraryOutlierCapper(max_capping_dict=None, min_capping_dict={'Age': 20},
+                                         missing_values='ignore')
+    X = transformer.fit_transform(dataframe_na)
+
+    df_transf = dataframe_na.copy()
+    df_transf['Age'] = np.where(df_transf['Age'] < 20, 20, df_transf['Age'])
+
+    # fit params
+    assert transformer.max_capping_dict is None
+    assert transformer.min_capping_dict == {'Age': 20}
+    assert transformer.input_shape_ == (8, 6)
+    # transform params
+    pd.testing.assert_frame_equal(X, df_transf)
+    assert X['Age'].min() >= 20
+    assert dataframe_na['Age'].min() < 20
+
     with pytest.raises(ValueError):
         ArbitraryOutlierCapper(max_capping_dict='other')
 
@@ -206,15 +249,18 @@ def test_ArbitraryOutlierCapper(dataframe_normal_dist, dataframe_na, dataframe_v
     with pytest.raises(ValueError):
         ArbitraryOutlierCapper(min_capping_dict=None, max_capping_dict=None)
 
+    with pytest.raises(ValueError):
+        ArbitraryOutlierCapper(missing_values='other')
+
     df_na = dataframe_normal_dist.copy()
     df_na.loc[1, 'var'] = np.nan
 
-    # test case 4: when dataset contains na, fit method
+    # test case 5: when dataset contains na, fit method
     with pytest.raises(ValueError):
         transformer = ArbitraryOutlierCapper(min_capping_dict={'var': -0.17486039103044})
         transformer.fit(df_na)
 
-    # test case 8: when dataset contains na, transform method
+    # test case 6: when dataset contains na, transform method
     with pytest.raises(ValueError):
         transformer = ArbitraryOutlierCapper(min_capping_dict={'var': -0.17486039103044})
         transformer.fit(dataframe_normal_dist)
@@ -225,7 +271,7 @@ def test_ArbitraryOutlierCapper(dataframe_normal_dist, dataframe_na, dataframe_v
         transformer.transform(dataframe_vartypes)
 
 
-def test_OutlierTrimmer(dataframe_normal_dist):
+def test_OutlierTrimmer(dataframe_normal_dist, dataframe_na):
     # test case 1: mean and std, right tail
     transformer = OutlierTrimmer(distribution='gaussian', tail='right', fold=1)
     X = transformer.fit_transform(dataframe_normal_dist)
@@ -253,3 +299,15 @@ def test_OutlierTrimmer(dataframe_normal_dist):
 
     pd.testing.assert_frame_equal(X, df_transf)
     assert len(X) == 98
+
+    # test case 4: dataset contains na, and transformer is asked to ignore
+    transformer = OutlierTrimmer(distribution='gaussian', tail='right', fold=1,
+                                 variables=['Age'], missing_values='ignore')
+    X = transformer.fit_transform(dataframe_na)
+
+    df_transf = dataframe_na.copy()
+    outliers = np.where(df_transf['Age'] > 38.79255087111844, True, False)
+    df_transf = df_transf.loc[~outliers]
+
+    pd.testing.assert_frame_equal(X, df_transf)
+    assert len(X) == 6