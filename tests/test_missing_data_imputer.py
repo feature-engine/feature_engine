043389d12fced8b1b2fdd3e9764e1c6ec89e3e34
Gleb Levitskiy <36483986+GLevV@users.noreply.github.com>
2020-07-29 15:46:47 +0300

    Update test_missing_data_imputer.py

--- tests/test_missing_data_imputer.py
+++ tests/test_missing_data_imputer.py
@@ -206,55 +206,8 @@ def test_CategoricalVariableImputer(dataframe_na):
     assert X_transformed[['Name', 'City', 'Studies']].isnull().sum().sum() == 0
     assert X_transformed[['Age', 'Marks']].isnull().sum().sum() > 0
     pd.testing.assert_frame_equal(X_transformed, ref_df)
-
-    # test case 2: mode imputation + user indicates 1 variable ONLY
-    imputer = CategoricalVariableImputer(imputation_method='frequent', variables='City')
-    X_transformed = imputer.fit_transform(dataframe_na)
-
-    ref_df = dataframe_na.copy()
-    ref_df['City'] = ref_df['City'].fillna('London')
-
-    assert imputer.imputation_method == 'frequent'
-    assert imputer.variables == ['City']
-    assert imputer.input_shape_ == (8, 6)
-    assert imputer.imputer_dict_ == {'City': 'London'}
-    assert X_transformed['City'].isnull().sum() == 0
-    assert X_transformed[['Age', 'Marks']].isnull().sum().sum() > 0
-    pd.testing.assert_frame_equal(X_transformed, ref_df)
-
-    # test case 3: mode imputation + user indicates multiple variables
-    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['Studies', 'City'])
-    X_transformed = imputer.fit_transform(dataframe_na)
-
-    ref_df = dataframe_na.copy()
-    ref_df['City'] = ref_df['City'].fillna('London')
-    ref_df['Studies'] = ref_df['Studies'].fillna('Bachelor')
-
-    assert imputer.imputer_dict_ == {'Studies': 'Bachelor', 'City': 'London'}
-    pd.testing.assert_frame_equal(X_transformed, ref_df)
-
-    # test case 4: imputing of numerical variables cast as object + return numeric
-    dataframe_na['Marks'] = dataframe_na['Marks'].astype('O')
-    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['City', 'Studies', 'Marks'])
-    X_transformed = imputer.fit_transform(dataframe_na)
-
-    ref_df = dataframe_na.copy()
-    ref_df['Marks'] = ref_df['Marks'].fillna(0.8)
-    ref_df['City'] = ref_df['City'].fillna('London')
-    ref_df['Studies'] = ref_df['Studies'].fillna('Bachelor')
-    assert imputer.variables == ['City', 'Studies', 'Marks']
-    assert imputer.imputer_dict_ == {'Studies': 'Bachelor', 'City': 'London', 'Marks': 0.8}
-    assert X_transformed['Marks'].dtype == 'float'
-    pd.testing.assert_frame_equal(X_transformed, ref_df)
-
-    # test case 5: imputing of numerical variables cast as object + return as object after imputation
-    dataframe_na['Marks'] = dataframe_na['Marks'].astype('O')
-    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['City', 'Studies', 'Marks'],
-                                         return_object=True)
-    X_transformed = imputer.fit_transform(dataframe_na)
-    assert X_transformed['Marks'].dtype == 'O'
     
-    # test case 6: imputing custom user-defined string + automatically select variables
+    # test case 2: imputing custom user-defined string + automatically select variables
     imputer = CategoricalVariableImputer(imputation_method='missing', fill_value='Unknown', variables=None)
     X_transformed = imputer.fit_transform(dataframe_na)
 
@@ -274,6 +227,53 @@ def test_CategoricalVariableImputer(dataframe_na):
     assert X_transformed[['Name', 'City', 'Studies']].isnull().sum().sum() == 0
     assert X_transformed[['Age', 'Marks']].isnull().sum().sum() > 0
     pd.testing.assert_frame_equal(X_transformed, ref_df)
+
+    # test case 3: mode imputation + user indicates 1 variable ONLY
+    imputer = CategoricalVariableImputer(imputation_method='frequent', variables='City')
+    X_transformed = imputer.fit_transform(dataframe_na)
+
+    ref_df = dataframe_na.copy()
+    ref_df['City'] = ref_df['City'].fillna('London')
+
+    assert imputer.imputation_method == 'frequent'
+    assert imputer.variables == ['City']
+    assert imputer.input_shape_ == (8, 6)
+    assert imputer.imputer_dict_ == {'City': 'London'}
+    assert X_transformed['City'].isnull().sum() == 0
+    assert X_transformed[['Age', 'Marks']].isnull().sum().sum() > 0
+    pd.testing.assert_frame_equal(X_transformed, ref_df)
+
+    # test case 4: mode imputation + user indicates multiple variables
+    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['Studies', 'City'])
+    X_transformed = imputer.fit_transform(dataframe_na)
+
+    ref_df = dataframe_na.copy()
+    ref_df['City'] = ref_df['City'].fillna('London')
+    ref_df['Studies'] = ref_df['Studies'].fillna('Bachelor')
+
+    assert imputer.imputer_dict_ == {'Studies': 'Bachelor', 'City': 'London'}
+    pd.testing.assert_frame_equal(X_transformed, ref_df)
+
+    # test case 5: imputing of numerical variables cast as object + return numeric
+    dataframe_na['Marks'] = dataframe_na['Marks'].astype('O')
+    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['City', 'Studies', 'Marks'])
+    X_transformed = imputer.fit_transform(dataframe_na)
+
+    ref_df = dataframe_na.copy()
+    ref_df['Marks'] = ref_df['Marks'].fillna(0.8)
+    ref_df['City'] = ref_df['City'].fillna('London')
+    ref_df['Studies'] = ref_df['Studies'].fillna('Bachelor')
+    assert imputer.variables == ['City', 'Studies', 'Marks']
+    assert imputer.imputer_dict_ == {'Studies': 'Bachelor', 'City': 'London', 'Marks': 0.8}
+    assert X_transformed['Marks'].dtype == 'float'
+    pd.testing.assert_frame_equal(X_transformed, ref_df)
+
+    # test case 6: imputing of numerical variables cast as object + return as object after imputation
+    dataframe_na['Marks'] = dataframe_na['Marks'].astype('O')
+    imputer = CategoricalVariableImputer(imputation_method='frequent', variables=['City', 'Studies', 'Marks'],
+                                         return_object=True)
+    X_transformed = imputer.fit_transform(dataframe_na)
+    assert X_transformed['Marks'].dtype == 'O'
 
     with pytest.raises(ValueError):
         imputer = CategoricalVariableImputer(imputation_method='arbitrary')
