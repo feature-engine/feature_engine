e9e0149d3a47132985a5d85bc3397e48efaedff2
Tejash-Shah <stejash15@gmail.com>
2020-09-15 23:12:04 -0700

    add tests for feature selector

--- tests/test_feature_selection.py
+++ tests/test_feature_selection.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from feature_engine.feature_selection import DropFeatures
+from feature_engine.feature_selection import DropFeatures, SelectFeatures
 
 
 def test_drop_features_drop_2_variables(dataframe_vartypes):
@@ -50,4 +50,89 @@ def test_drop_features_empty_list(dataframe_vartypes):
     with pytest.raises(ValueError):
         transformer = DropFeatures(features_to_drop=[])
         transformer.fit_transform(dataframe_vartypes)
+
+
+# SelectFeatures transformer tests
+def test_select_features_drop_constant_features(dataframe_con_quasi_con):
+    # test case 1: only drop constant features
+    transformer = SelectFeatures(drop_quasi_constant_features=False, quasi_constant_threshold=None)
+
+    X = transformer.fit_transform(dataframe_con_quasi_con)
+
+    # expected result
+    df = pd.DataFrame(
+        {'Name': ['tom', 'nick', 'krish', 'jack'],
+         'City': ['London', 'Manchester', 'Liverpool', 'Bristol'],
+         'Age': [20, 21, 19, 18],
+         'Marks': [0.9, 0.8, 0.7, 0.6],
+         'dob': pd.date_range('2020-02-24', periods=4, freq='T'),
+         'quasi_feat_num': [1, 1, 1, 2],
+         'quasi_feat_cat': ['a', 'a', 'a', 'b']}
+    )
+
+    # init params
+    assert transformer.drop_quasi_constant_features is False
+    assert transformer.quasi_constant_threshold is None
+    # transform params
+    assert X.shape == (4, 7)
+    assert type(X) == pd.DataFrame
+    pd.testing.assert_frame_equal(X, df)
+
+
+def test_select_features_drop_quasi_constant_features(dataframe_con_quasi_con):
+    # test case 2: drop quasi constant features (also by default drops constant features)
+    transformer = SelectFeatures(drop_quasi_constant_features=True, quasi_constant_threshold=0.7)
+
+    X = transformer.fit_transform(dataframe_con_quasi_con)
+
+    # expected result
+    df = pd.DataFrame(
+        {'Name': ['tom', 'nick', 'krish', 'jack'],
+         'City': ['London', 'Manchester', 'Liverpool', 'Bristol'],
+         'Age': [20, 21, 19, 18],
+         'Marks': [0.9, 0.8, 0.7, 0.6],
+         'dob': pd.date_range('2020-02-24', periods=4, freq='T'),
+         }
+    )
+
+    # init params
+    assert transformer.drop_quasi_constant_features is True
+    assert transformer.quasi_constant_threshold == 0.7
+    # transform params
+    assert X.shape == (4, 5)
+    assert type(X) == pd.DataFrame
+    pd.testing.assert_frame_equal(X, df)
+
+
+def test_select_features_input_not_dataframe():
+    # test case 3: input is not a dataframe
+    with pytest.raises(TypeError):
+        SelectFeatures().fit({"Name": ["Karthik"]})
+
+
+def test_select_features_threshold_out_of_range():
+    # test case 4: threshold more than 1 or less than 0
+    with pytest.raises(ValueError):
+        SelectFeatures(drop_quasi_constant_features=True, quasi_constant_threshold=2)
+
+    with pytest.raises(ValueError):
+        SelectFeatures(drop_quasi_constant_features=True, quasi_constant_threshold=-1)
+
+    with pytest.raises(ValueError):
+        SelectFeatures(drop_quasi_constant_features=True, quasi_constant_threshold=1)
+
+
+def test_select_features_all_constant_features():
+    # test case 5: when input contains all constant features
+    with pytest.raises(ValueError):
+        SelectFeatures().fit(pd.DataFrame({'col1': [1, 1, 1], 'col2': [1, 1, 1]}))
+
+
+def test_select_features_all_constant_and_quasi_constant_features():
+    # test case 6: when input contains all constant and quasi constant features
+    with pytest.raises(ValueError):
+        transformer = SelectFeatures(drop_quasi_constant_features=True, quasi_constant_threshold=0.7)
+        transformer.fit_transform(pd.DataFrame({'col1': [1, 1, 1, 1], 'col2': [1, 1, 1, 1],
+                                                'col3': [1, 1, 1, 2], 'col4': [1, 1, 1, 2]
+                                                }))
 
