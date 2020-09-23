a3e4ed6e508d6e1fb0f7b8f9be07ceb395eeb256
Tejash-Shah <stejash15@gmail.com>
2020-09-22 20:48:22 -0700

    updated DropConstantAndQuasiConstant transformer

--- feature_engine/feature_selection.py
+++ feature_engine/feature_selection.py
@@ -1,8 +1,8 @@
-import warnings
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 from feature_engine.dataframe_checks import _is_dataframe, _check_input_matches_training_df
-from feature_engine.variable_manipulation import _define_variables
+from feature_engine.variable_manipulation import _define_variables, _find_all_variables
 
 
 class DropFeatures(BaseEstimator, TransformerMixin):
@@ -89,60 +89,40 @@ class DropFeatures(BaseEstimator, TransformerMixin):
         return X
 
 
-class SelectFeatures(TransformerMixin, BaseEstimator):
+class DropConstantAndQuasiConstantFeatures(TransformerMixin, BaseEstimator):
     """
-    SelectFeatures()
 
     Drops constant and quasi constant variables from a dataframe.
     Constant variables are those variables which show only one value for all the observations.
     By default, all constant variables are dropped.
-    Quasi-constant variables are those variables which are almost constant. In other words, those features have the same
-    value for large subset of total observations.
-    This transformer works for both, numerical and categorical variables.
+    Quasi-constant variables are those variables which are almost constant. In other words, those features which have
+    the same value for a large subset of total observations.
+    This transformer works for both numerical and categorical variables.
 
     Parameters
     ----------
 
-    drop_quasi_constant_features : bool, default=False
-        Whether to drop quasi constant features
+    tol: float, default=1
+        Threshold to detect constant/quasi-constant features. Variables showing tol percentage of values will be dropped
 
-    quasi_constant_threshold: float, default=None
-        When drop_quasi_constant_features is True, features showing quasi_constant_threshold percentage of values
-        will be dropped
+    variables: list, default=None
+        The list of variables to inspect. If None, the transformer will select all variables in the dataframe
 
     Attributes
     ----------
-
-    constant_features_: list
-        List of constant features in a dataframe
-
-    quasi_constant_features_: list
-        List of quasi constant features in a dataframe
 
     selected_features_: list
         List of selected features which are non-constant and non-quasi constant
 
     """
-    def __init__(self, drop_quasi_constant_features=False, quasi_constant_threshold=None):
 
-        self.drop_quasi_constant_features = drop_quasi_constant_features
-        self.quasi_constant_threshold = quasi_constant_threshold
+    def __init__(self, tol=1, variables=None):
 
-        self.constant_features_ = []
-        self.selected_features_ = []
-        self.quasi_constant_features_ = []
-        self._input_shape_ = ()
+        if tol < 0 or tol > 1:
+            raise ValueError("tol takes values between 0 and 1")
 
-        if not self.drop_quasi_constant_features and self.quasi_constant_threshold is not None:
-            warnings.warn(
-                'Setting a quasi_constant_threshold has no effect since drop_quasi_constant_features is False. You '
-                'should leave quasi_constant_threshold to its default (None), or set drop_quasi_constant_features=True.'
-                , FutureWarning
-            )
-
-        if self.drop_quasi_constant_features:
-            if self.quasi_constant_threshold <= 0 or self.quasi_constant_threshold >= 1:
-                raise ValueError("Value of threshold should be between 0 and 1")
+        self.tol = tol
+        self.variables = _define_variables(variables)
 
     def fit(self, X, y=None):
 
@@ -160,35 +140,42 @@ class SelectFeatures(TransformerMixin, BaseEstimator):
 
         """
 
-        # data frame check
+        # check input dataframe
         X = _is_dataframe(X)
 
-        # get input shape
-        self._input_shape_ = X.shape
+        # find all variables or check those entered are present in the dataframe
+        self.variables = _find_all_variables(X, self.variables)
 
-        # constant features
-        self.constant_features_ = [feat for feat in X.columns if X[feat].nunique() == 1]
+        if self.tol == 1:
 
-        # non-constant features
-        self.selected_features_ = [feat for feat in X.columns if feat not in self.constant_features_]
+            # constant features
+            constant_features = [feat for feat in self.variables if X[feat].nunique() == 1]
 
-        # if total constant features is equal to total features raise an error
-        if len(self.constant_features_) == len(X.columns):
-            raise ValueError("The resulting dataframe will have no columns after dropping all constant features")
+            self.selected_features_ = [feat for feat in X.columns if feat not in constant_features]
 
-        if self.drop_quasi_constant_features:
-            # quasi-constant features
-            self.quasi_constant_features_ = [feat for feat in X.columns if X[feat].value_counts(
-                normalize=True, sort=True, ascending=False).iloc[0] > self.quasi_constant_threshold
-                                             and feat not in self.constant_features_
-                                             and feat in self.selected_features_]
+            # if total constant features is equal to total features raise an error
+            if len(constant_features) == len(X.columns):
+                raise ValueError("The resulting dataframe will have no columns after dropping all constant features")
 
-            self.selected_features_ = [feat for feat in self.selected_features_
-                                       if feat not in self.quasi_constant_features_]
+        else:
+            constant_plus_quasi_constant_features = []
+            self.selected_features_ = []
 
-        if len(self.constant_features_) + len(self.quasi_constant_features_) == len(X.columns):
-            raise ValueError("The resulting dataframe will have no columns after dropping all constant "
-                             "and quasi constant features")
+            for feat in self.variables:
+
+                predominant = (X[feat].value_counts() / np.float(len(X))).sort_values(ascending=False).values[0]
+
+                if predominant > self.tol:
+                    constant_plus_quasi_constant_features.append(feat)
+
+            # if total constant features is equal to total features raise an error
+            if len(constant_plus_quasi_constant_features) == len(X.columns):
+                raise ValueError("The resulting dataframe will have no columns after dropping "
+                                 "constant and quasi constant features")
+
+            self.selected_features_ = [feat for feat in X.columns if feat not in constant_plus_quasi_constant_features]
+
+        self.input_shape_ = X.shape
 
         return self
 
@@ -216,11 +203,9 @@ class SelectFeatures(TransformerMixin, BaseEstimator):
         X = _is_dataframe(X)
 
         # check if number of columns in test dataset matches to train dataset
-        _check_input_matches_training_df(X, self._input_shape_[1])
+        _check_input_matches_training_df(X, self.input_shape_[1])
 
         # returned selected features
         X = X[self.selected_features_].copy()
 
         return X
-
-
