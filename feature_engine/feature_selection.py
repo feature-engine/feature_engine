7f52cffadeab804fc701801999025ec1a715c452
thibault <thibault.blanc@yahoo.fr>
2020-10-01 21:57:13 +0200

    Added SimilarColumns (Automatic handle of columns difference in test and train)

--- feature_engine/feature_selection.py
+++ feature_engine/feature_selection.py
@@ -88,6 +88,63 @@ class DropFeatures(BaseEstimator, TransformerMixin):
 
         return X
 
+class SimilarColumns(BaseEstimator, TransformerMixin):
+    """ Ensure that similar columns are in test and train dataset.
+
+
+    Parameters
+    ----
+    None
+
+    """
+    def __init__(self):
+        self.col = None
+
+    def fit(self, df, y=None, **fit_params):
+        """  Fit columns schema
+
+        Parameters
+        ----------
+
+        X: pandas dataframe of shape = [n_samples, n_features]
+            The input dataframe
+
+        y: None
+            y is not needed for this transformer. You can pass y or None.
+
+        """
+
+        self.col = df.columns
+        return self
+
+    def transform(self, X, **transform_params):
+        """  Drops the variable that are not in the fited dataframe and returns a new dataframe with the remaining subset of variables.
+
+        If a column is in train but not in test, then the column will be created in
+        test dataset with missing value everywhere.
+
+        If a column is in test but not in train, it will be dropped. 
+
+        Parameters
+        ----------
+        X: pandas dataframe
+            The input dataframe from which features will be dropped
+
+        Returns
+        -------
+        X_transformed: pandas dataframe of shape = [n_samples, n_features - len(features_to_drop)]
+            The transformed dataframe with the same columns (in same order) than the fitted dataframe
+
+        """
+
+        for col in self.col:
+            if col not in X.columns:
+                X[col] = np.nan
+        # reorder columns
+        X = X.loc[:, self.col]
+        return X
+
+
 
 class DropConstantFeatures(TransformerMixin, BaseEstimator):
     """
