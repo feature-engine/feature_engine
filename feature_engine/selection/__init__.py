e98b970b7defcbe0837b70d923bf3028268bfd01
Tejash-Shah <stejash15@gmail.com>
2020-10-03 15:40:58 -0700

    add DropDuplicateFeatures in init

--- feature_engine/selection/__init__.py
+++ feature_engine/selection/__init__.py
@@ -4,8 +4,10 @@ The module selection includes classes to select features or remove unwanted feat
 
 from .drop_features import DropFeatures
 from .drop_constant_features import DropConstantFeatures
+from .drop_duplicate_features import DropDuplicateFeatures
 
 __all__ = [
     'DropFeatures',
-    'DropConstantFeatures'
+    'DropConstantFeatures',
+    'DropDuplicateFeatures'
 ]
