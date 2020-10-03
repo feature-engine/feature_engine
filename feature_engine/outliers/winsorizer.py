9c4ab0b8fbeb9107ab019e60aebefa85140f6ecb
Soledad Galli <solegalli@protonmail.com>
2020-10-03 16:51:54 +0200

    create baseOutlier

--- feature_engine/outliers/winsorizer.py
+++ feature_engine/outliers/winsorizer.py
@@ -1,23 +1,12 @@
 # Authors: Soledad Galli <solegalli@protonmail.com>
 # License: BSD 3 clause
 
-import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils.validation import check_is_fitted
-
-from feature_engine.variable_manipulation import (
-    _define_variables,
-    _find_numerical_variables
-)
-
-from feature_engine.dataframe_checks import (
-    _is_dataframe,
-    _check_input_matches_training_df,
-    _check_contains_na,
-)
+from feature_engine.dataframe_checks import _is_dataframe, _check_contains_na
+from feature_engine.outliers.base_outlier import BaseOutlier
+from feature_engine.variable_manipulation import _define_variables, _find_numerical_variables
 
 
-class Winsorizer(BaseEstimator, TransformerMixin):
+class Winsorizer(BaseOutlier):
     """
     The Winsorizer() caps maximum and / or minimum values of a variable.
     
@@ -195,44 +184,11 @@ class Winsorizer(BaseEstimator, TransformerMixin):
 
         return self
 
+    # Ugly work around to import the docstring for Sphinx, otherwise none of this is necessary
     def transform(self, X):
-        """
-        Caps the variable values, that is, censors outliers.
-
-        Parameters
-        ----------
-
-        X : pandas dataframe of shape = [n_samples, n_features]
-            The data to be transformed.
-
-        Returns
-        -------
-
-        X_transformed : pandas dataframe of shape = [n_samples, n_features]
-            The dataframe with the capped variables.
-        """
-
-        # check input dataframe an if class was fitted
-        check_is_fitted(self)
-
-        # check that input is a dataframe
-        X = _is_dataframe(X)
-
-        if self.missing_values == 'raise':
-            # check if dataset contains na
-            _check_contains_na(X, self.variables)
-
-        # Check that the dataframe contains the same number of columns
-        # than the dataframe used to fit the imputer.
-        _check_input_matches_training_df(X, self.input_shape_[1])
-
-        for feature in self.right_tail_caps_.keys():
-            X[feature] = np.where(X[feature] > self.right_tail_caps_[feature], self.right_tail_caps_[feature],
-                                  X[feature])
-
-        for feature in self.left_tail_caps_.keys():
-            X[feature] = np.where(X[feature] < self.left_tail_caps_[feature], self.left_tail_caps_[feature], X[feature])
-
+        X = super().transform(X)
         return X
+
+    transform.__doc__ = BaseOutlier.transform.__doc__
 
 
