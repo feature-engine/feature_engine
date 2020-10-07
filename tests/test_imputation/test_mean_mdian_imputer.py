ea2fcbc5b3791fb3bf8eaa6b3e2a3bf92880c314
Soledad Galli <solegalli@protonmail.com>
2020-10-07 15:31:08 +0200

    split tests into individual functions (#147)
    * split imputation tests
    
    * fix typo, unify test names in math combination test
    
    * reformat line space in sklearwrapper test
    
    * remove comment in drop constant tests
    
    * split discretisation tests
    
    * split encoding tests
    
    * split test outliers
    
    * split transformer tests

--- tests/test_imputation/test_mean_mdian_imputer.py
+++ tests/test_imputation/test_mean_mdian_imputer.py
@@ -1,56 +1,63 @@
 import pandas as pd
 import pytest
 from sklearn.exceptions import NotFittedError
-
 from feature_engine.imputation import MeanMedianImputer
 
 
-def test_MeanMedianImputer(dataframe_na):
-
-    # test case 1: automatically finds numerical variables
+def test_mean_imputation_and_automatically_select_variables(dataframe_na):
+    # set up transformer
     imputer = MeanMedianImputer(imputation_method='mean', variables=None)
     X_transformed = imputer.fit_transform(dataframe_na)
 
-    ref_df = dataframe_na.copy()
-    ref_df['Age'] = ref_df['Age'].fillna(28.714285714285715)
-    ref_df['Marks'] = ref_df['Marks'].fillna(0.6833333333333332)
+    # set up reference result
+    X_reference = dataframe_na.copy()
+    X_reference['Age'] = X_reference['Age'].fillna(28.714285714285715)
+    X_reference['Marks'] = X_reference['Marks'].fillna(0.6833333333333332)
 
-    # check init params
+    # test init params
     assert imputer.imputation_method == 'mean'
     assert imputer.variables == ['Age', 'Marks']
 
-    # check fit attributes
+    # test fit attributes
     assert imputer.imputer_dict_ == {'Age': 28.714285714285715, 'Marks': 0.6833333333333332}
     assert imputer.input_shape_ == (8, 6)
 
-    # check transform output: indicated variables no NA
-    # Not indicated variables still have NA
+    # test transform output:
+    # selected variables should have no NA
+    # not selected variables should still have NA
     assert X_transformed[['Age', 'Marks']].isnull().sum().sum() == 0
     assert X_transformed[['Name', 'City']].isnull().sum().sum() > 0
-    pd.testing.assert_frame_equal(X_transformed, ref_df)
+    pd.testing.assert_frame_equal(X_transformed, X_reference)
 
-    # test case 2: single user determined variable
+
+def test_median_imputation_when_user_enters_single_variables(dataframe_na):
+    # set up trasnformer
     imputer = MeanMedianImputer(imputation_method='median', variables=['Age'])
     X_transformed = imputer.fit_transform(dataframe_na)
 
-    ref_df = dataframe_na.copy()
-    ref_df['Age'] = ref_df['Age'].fillna(23.0)
+    # set up reference output
+    X_reference = dataframe_na.copy()
+    X_reference['Age'] = X_reference['Age'].fillna(23.0)
 
-    # init params
+    # test init params
     assert imputer.imputation_method == 'median'
     assert imputer.variables == ['Age']
 
-    # fit params
+    # test fit attributes
     assert imputer.input_shape_ == (8, 6)
     assert imputer.imputer_dict_ == {'Age': 23.0}
 
-    # transform params
+    # test transform output
     assert X_transformed['Age'].isnull().sum() == 0
-    pd.testing.assert_frame_equal(X_transformed, ref_df)
+    pd.testing.assert_frame_equal(X_transformed, X_reference)
 
+
+def test_raises_error_with_wrong_imputation_method_parameter():
     with pytest.raises(ValueError):
         MeanMedianImputer(imputation_method='arbitrary')
 
+
+def test_raises_non_fitted_error(dataframe_na):
     with pytest.raises(NotFittedError):
         imputer = MeanMedianImputer()
         imputer.transform(dataframe_na)
--- tests/test_imputation/test_random_sample_imputer.py
+++ tests/test_imputation/test_random_sample_imputer.py
@@ -18,12 +18,12 @@ def test_define_seed(dataframe_vartypes):
     assert _define_seed(dataframe_vartypes, 3, ['Marks'], how='multiply') == 1
 
 
-def test_RandomSampleImputer(dataframe_na):
-
-    # test case 1: imputer with general seed + automatically select variables
+def test_imputer_with_general_seed_plus_automatically_select_variables(dataframe_na):
+    # set up transformer
     imputer = RandomSampleImputer(variables=None, random_state=5, seed='general')
     X_transformed = imputer.fit_transform(dataframe_na)
 
+    # expected output:
     # fillna based on seed used (found experimenting on Jupyter notebook)
     ref = {'Name': ['tom', 'nick', 'krish', 'peter', 'peter', 'sam', 'fred', 'sam'],
            'City': ['London', 'Manchester', 'London', 'Manchester', 'London', 'London', 'Bristol', 'Manchester'],
@@ -33,23 +33,28 @@ def test_RandomSampleImputer(dataframe_na):
            'dob': pd.date_range('2020-02-24', periods=8, freq='T')
            }
     ref = pd.DataFrame(ref)
-    # init params
+
+    # test init params
     assert imputer.variables == ['Name', 'City', 'Studies', 'Age', 'Marks', 'dob']
     assert imputer.random_state == 5
     assert imputer.seed == 'general'
-    # fit params
+
+    # test fit attr
     assert imputer.input_shape_ == (8, 6)
     pd.testing.assert_frame_equal(imputer.X_, dataframe_na)
-    # transform output
+
+    # test transform output
     pd.testing.assert_frame_equal(X_transformed, ref, check_dtype=False)
 
-    # test case 2: imputer sed per observation using multiple variables to determine the random_state
+
+def test_imputer_with_seed_per_observation_with_multiple_variables_to_determine_random_state(dataframe_na):
+    # test case 2: imputer seed per observation using multiple variables to determine the random_state
     # Note the variables used as seed should not have missing data
     imputer = RandomSampleImputer(variables=['City', 'Studies'], random_state=['Marks', 'Age'], seed='observation')
     dataframe_na[['Marks', 'Age']] = dataframe_na[['Marks', 'Age']].fillna(1)
     X_transformed = imputer.fit_transform(dataframe_na)
 
-    # how the imputed dataframe should look like
+    # expected output
     ref = {'Name': ['tom', 'nick', 'krish', np.nan, 'peter', np.nan, 'fred', 'sam'],
            'City': ['London', 'Manchester', 'London', 'London', 'London', 'London', 'Bristol', 'Manchester'],
            'Studies': ['Bachelor', 'Bachelor', 'PhD', 'Bachelor', 'Bachelor', 'PhD', 'None', 'Masters'],
@@ -65,12 +70,15 @@ def test_RandomSampleImputer(dataframe_na):
     pd.testing.assert_frame_equal(imputer.X_[['City', 'Studies']], dataframe_na[['City', 'Studies']])
     pd.testing.assert_frame_equal(X_transformed[['City', 'Studies']], ref[['City', 'Studies']])
 
+
+def test_imputer_with_seed_per_observation_plus_product_of_seeding_variables(dataframe_na):
     # test case 3: observation seed, 2 variables as seed, product of seed variables
     imputer = RandomSampleImputer(variables=['City', 'Studies'], random_state=['Marks', 'Age'], seed='observation',
                                   seeding_method='multiply')
     dataframe_na[['Marks', 'Age']] = dataframe_na[['Marks', 'Age']].fillna(1)
     X_transformed = imputer.fit_transform(dataframe_na)
 
+    # expected output
     ref = {'Name': ['tom', 'nick', 'krish', np.nan, 'peter', np.nan, 'fred', 'sam'],
            'City': ['London', 'Manchester', 'London', 'Manchester', 'London', 'London', 'Bristol', 'Manchester'],
            'Studies': ['Bachelor', 'Bachelor', 'Bachelor', 'Masters', 'Bachelor', 'PhD', 'None', 'Masters'],
@@ -86,12 +94,15 @@ def test_RandomSampleImputer(dataframe_na):
     pd.testing.assert_frame_equal(imputer.X_[['City', 'Studies']], dataframe_na[['City', 'Studies']])
     pd.testing.assert_frame_equal(X_transformed[['City', 'Studies']], ref[['City', 'Studies']], check_dtype=False)
 
+
+def test_seed_per_observation_with_only_1_variable_as_seed(dataframe_na):
     # test case 4: observation seed, only variable indicated as seed, method: addition
     # Note the variable used as seed should not have missing data
     imputer = RandomSampleImputer(variables=['City', 'Studies'], random_state='Age', seed='observation')
     dataframe_na['Age'] = dataframe_na['Age'].fillna(1)
     X_transformed = imputer.fit_transform(dataframe_na)
 
+    # expected output
     ref = {'Name': ['tom', 'nick', 'krish', np.nan, 'peter', np.nan, 'fred', 'sam'],
            'City': ['London', 'Manchester', 'Manchester', 'Manchester', 'London', 'London', 'Bristol', 'Manchester'],
            'Studies': ['Bachelor', 'Bachelor', 'Masters', 'Masters', 'Bachelor', 'PhD', 'None', 'Masters'],
@@ -100,18 +111,33 @@ def test_RandomSampleImputer(dataframe_na):
            'dob': pd.date_range('2020-02-24', periods=8, freq='T')
            }
     ref = pd.DataFrame(ref)
+
     assert imputer.random_state == ['Age']
     pd.testing.assert_frame_equal(imputer.X_[['City', 'Studies']], dataframe_na[['City', 'Studies']])
     pd.testing.assert_frame_equal(X_transformed[['City', 'Studies']], ref[['City', 'Studies']], check_dtype=False)
 
+
+def test_raise_error_if_seed_not_permitted_value():
     with pytest.raises(ValueError):
         RandomSampleImputer(seed='arbitrary')
+
+
+def test_raises_error_if_seeding_method_not_permitted_value():
     with pytest.raises(ValueError):
         RandomSampleImputer(seeding_method='arbitrary')
+
+
+def test_raises_error_if_random_state_takes_not_permitted_value():
     with pytest.raises(ValueError):
         RandomSampleImputer(seed='general', random_state='arbitrary')
+
+
+def test_raises_error_if_random_state_is_none_when_seed_is_observation():
     with pytest.raises(ValueError):
         RandomSampleImputer(seed='observation', random_state=None)
+
+
+def test_raises_error_if_random_state_is_string(dataframe_na):
     with pytest.raises(ValueError):
         imputer = RandomSampleImputer(seed='observation', random_state='arbitrary')
         imputer.fit(dataframe_na)
