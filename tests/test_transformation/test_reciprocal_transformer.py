9bb14ba4bc05e0dd9447d148a5bfa3f36216159c
Soledad Galli <solegalli@protonmail.com>
2020-10-07 15:19:50 +0200

    split transformer tests

--- tests/test_transformation/test_reciprocal_transformer.py
+++ tests/test_transformation/test_reciprocal_transformer.py
@@ -5,34 +5,40 @@ from sklearn.exceptions import NotFittedError
 from feature_engine.transformation import ReciprocalTransformer
 
 
-def test_ReciprocalTransformer(dataframe_vartypes, dataframe_na):
+def test_automatically_find_variables(dataframe_vartypes):
     # test case 1: automatically select variables
     transformer = ReciprocalTransformer(variables=None)
     X = transformer.fit_transform(dataframe_vartypes)
 
-    # transformed dataframe
+    # expected output
     transf_df = dataframe_vartypes.copy()
     transf_df['Age'] = [0.05, 0.047619, 0.0526316, 0.0555556]
     transf_df['Marks'] = [1.11111, 1.25, 1.42857, 1.66667]
 
-    # init params
+    # test init params
     assert transformer.variables == ['Age', 'Marks']
-    # fit params
+    # test fit attr
     assert transformer.input_shape_ == (4, 5)
-    # transform params
+    # test transform output
     pd.testing.assert_frame_equal(X, transf_df)
 
+
+def test_fit_raises_error_if_na_in_df(dataframe_na):
     # test case 2: when dataset contains na, fit method
     with pytest.raises(ValueError):
         transformer = ReciprocalTransformer()
         transformer.fit(dataframe_na)
 
+
+def test_transform_raises_error_if_na_in_df(dataframe_vartypes, dataframe_na):
     # test case 3: when dataset contains na, transform method
     with pytest.raises(ValueError):
         transformer = ReciprocalTransformer()
         transformer.fit(dataframe_vartypes)
         transformer.transform(dataframe_na[['Name', 'City', 'Age', 'Marks', 'dob']])
 
+
+def test_raises_error_if_df_contains_0_as_value(dataframe_vartypes):
     # test error when data contains value zero
     df_neg = dataframe_vartypes.copy()
     df_neg.loc[1, 'Age'] = 0
@@ -48,6 +54,8 @@ def test_ReciprocalTransformer(dataframe_vartypes, dataframe_na):
         transformer.fit(dataframe_vartypes)
         transformer.transform(df_neg)
 
+
+def test_raises_non_fitted_error(dataframe_vartypes):
     with pytest.raises(NotFittedError):
         transformer = ReciprocalTransformer()
         transformer.transform(dataframe_vartypes)
