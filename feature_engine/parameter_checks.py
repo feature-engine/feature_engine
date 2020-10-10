5f13bf324e1e8940db610cf21724d57e3d582e68
Okroshiashvili <n.okroshiashvili@gmail.com>
2020-10-10 11:42:42 +0400

    add type hints and docstrings

--- feature_engine/parameter_checks.py
+++ feature_engine/parameter_checks.py
@@ -1,11 +1,30 @@
-def _define_numerical_dict(the_dict):
-    # Check that the entered dictionary is indeed a dictionary of integers and floats
-    # Can take None as value
-    if not the_dict:
-        the_dict = the_dict
-    elif isinstance(the_dict, dict):
-        if not all([isinstance(x, (float, int)) for x in the_dict.values()]):
+from typing import Optional
+
+
+def _define_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
+    """
+    Takes a dictionary and checks if all values in dictionary are integers and floats.
+    Can take None as argument.
+
+    Args:
+        the_dict: Dict to perform check against
+
+    Raises:
+        ValueError: If all values of dict are not int or float
+        TypeError: When argument type is not dict
+
+    Returns:
+        None or the dict
+    """
+
+    if not dict_:
+        dict_ = dict_
+
+    elif isinstance(dict_, dict):
+        if not all([isinstance(x, (float, int)) for x in dict_.values()]):
             raise ValueError("All values in the dictionary must be integer or float")
+
     else:
         raise TypeError("The parameter can only take a dictionary or None")
-    return the_dict
+
+    return dict_
