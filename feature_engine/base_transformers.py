6720789fb0e4a54522cca5f48a96f9c448652f81
Okroshiashvili <n.okroshiashvili@gmail.com>
2020-10-17 09:46:13 +0400

    update type hints and docstring

--- feature_engine/base_transformers.py
+++ feature_engine/base_transformers.py
@@ -19,15 +19,16 @@ class BaseNumericalTransformer(BaseEstimator, TransformerMixin):
     # shared set-up procedures across numerical transformers, i.e.,
     # variable transformers, discretisers, math combination
 
-    def fit(self, X: pd.DataFrame, y: Optional[str] = None) -> pd.DataFrame:
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> pd.DataFrame:
         """
         Fits the transformation to the DataFrame.
 
         Args:
             X: Pandas DataFrame to fit the transformation
+
             y: This parameter exists only for compatibility with
             sklearn.pipeline.Pipeline.
-            Defaults to None.
+            Defaults to None. Alternatively takes Pandas Series.
 
         Returns:
             DataFrame with fitted transformation
