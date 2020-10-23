ae111922344f724d6db78d5b016211f2b529619e
SunnyxBd <bdriss.sana@gmail.com>
2020-10-23 13:39:47 +0200

    updated code style to match the requirements

--- feature_engine/selection/shuffle_features.py
+++ feature_engine/selection/shuffle_features.py
@@ -2,14 +2,15 @@ import random
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 
-from feature_engine.dataframe_checks import (
-    _is_dataframe,
-    _check_input_matches_training_df,
-)
-from feature_engine.variable_manipulation import _define_variables, _find_all_variables
+from feature_engine.dataframe_checks import _is_dataframe, \
+    _check_input_matches_training_df
+
+from feature_engine.variable_manipulation import _define_variables, \
+    _find_all_variables
 
 
 class ShuffleFeatures(BaseEstimator, TransformerMixin):
+
     """
     
     ShuffleFeatures reorganizes the values inside each feature from a dataframe.
@@ -20,13 +21,13 @@ class ShuffleFeatures(BaseEstimator, TransformerMixin):
 
     features_to_shuffle : str or list, default=None
         The list of variable(s) to be shuffled from the dataframe
-        If None, the transformer will evaluate all variables in the dataset. #todo
+        If None, the transformer will shuffle all variables in the dataset.
     """
 
     def __init__(self, features_to_shuffle=None):
 
-        self.features_to_shuffle = _define_variables(features_to_shuffle)
-
+        self.features_to_shuffle = \
+            _define_variables(features_to_shuffle)
 
     def fit(self, X, y=None):
         """
@@ -50,16 +51,22 @@ class ShuffleFeatures(BaseEstimator, TransformerMixin):
         """
 
         # check input dataframe
+
         X = _is_dataframe(X)
-        
+
         # find all variables or check those entered are in the dataframe
-        self.features_to_shuffle = _find_all_variables(X, self.features_to_shuffle)
+
+        self.features_to_shuffle = _find_all_variables(X,
+                self.features_to_shuffle)
 
         # dict to collect features that are shuffled
+
         self.shuffled_features_ = {}
-        
+
+        # shuffle features and save into a dict
+
         for feature in self.features_to_shuffle:
-            
+
             shuffled_feature = random.sample(list(X[feature]), len(X))
 
             self.self.shuffled_features_[feature] = shuffled_feature
@@ -86,14 +93,15 @@ class ShuffleFeatures(BaseEstimator, TransformerMixin):
         """
 
         # check if fit is performed prior to transform
+
         check_is_fitted(self)
 
         # check if input is a dataframe
+
         X = _is_dataframe(X)
 
-        
-        ## Overwrite olf features with shuffled ones
+        # Overwrite old features with shuffled ones
+
         X = X.assign(**self.shuffled_features_)
-        
+
         return X
-    
