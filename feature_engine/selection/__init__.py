8b409e12f96ca4ca43ec5081f836cc5a379b180f
NicoGalli <72278140+NicoGalli@users.noreply.github.com>
2020-10-28 03:59:20 -0300

    Add Correlated feature selector and test cases #168 #161 #140 (#165)
    * remove correlated feature #NroIssue
    
    remove correlated features
    
    * Issue ### Test Correlated Features
    
    Test cases for Correlated Features
    
    * Issue# Correlated Feature - validate DF dtypes is numeric and others
    
    Raise TypeError if DF dtypes are not numeric.
    Test cases and DF updates.
    Please note that test cases calling fit_transform function and DataSet needs your review.
    
    * add correlated features selector
    
    * fix input_shape assert
    
    * shorten url
    
    * remove numpy import
    
    * Update test_drop_correlated_features.py
    
    Co-authored-by: Soledad Galli <solegalli@protonmail.com>

--- feature_engine/selection/__init__.py
+++ feature_engine/selection/__init__.py
@@ -5,5 +5,11 @@ The module selection includes classes to select features or remove unwanted feat
 from .drop_features import DropFeatures
 from .drop_constant_features import DropConstantFeatures
 from .drop_duplicate_features import DropDuplicateFeatures
+from .drop_correlated_features import DropCorrelatedFeatures
 
-__all__ = ["DropFeatures", "DropConstantFeatures", "DropDuplicateFeatures"]
+__all__ = [
+    "DropFeatures",
+    "DropConstantFeatures",
+    "DropDuplicateFeatures",
+    "DropCorrelatedFeatures",
+]
