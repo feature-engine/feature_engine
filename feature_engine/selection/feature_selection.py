e5f733ec6c419b85d383bab690acc3369790ec7f
NicoGalli <nicolas.galli@yahoo.com>
2020-11-02 17:46:03 -0300

    implements threshold and loop to call cross_validate #137

--- feature_engine/selection/feature_selection.py
+++ feature_engine/selection/feature_selection.py
@@ -40,6 +40,12 @@ class FeatureSelection(BaseEstimator, TransformerMixin):
         sklearn.metrics. See the model evaluation documentation for more options:
         https://scikit-learn.org/stable/modules/model_evaluation.html
 
+    threshold: float, int, default = 0.01
+        The value that defines if a feature will be kept or removed. Note that for
+        metrics like roc-auc, r2_score and accuracy, the thresholds will be floats
+        between 0 and 1. For metrics like the mean_square_error and the
+        root_mean_square_error the threshold will be a big number.
+        The threshold must be defined by the user.
 
     cv : int, default=3
         Desired number of cross-validation fold to be used to fit the estimator.
@@ -69,15 +75,20 @@ class FeatureSelection(BaseEstimator, TransformerMixin):
         estimator=RandomForestClassifier(),
         scoring="roc_auc",
         cv=3,
+        threshold=0.5,
         variables=None,
     ):
 
         if not isinstance(cv, int) or cv < 1:
             raise ValueError("cv can only take positive integers bigger than 1")
 
+        if not isinstance(threshold, (int, float)):
+            raise ValueError("threshold can only be integer or float")
+
         self.variables = _define_variables(variables)
         self.estimator = estimator
         self.scoring = scoring
+        self.threshold = threshold
         self.cv = cv
 
     def fit(self, X, y):
@@ -109,30 +120,22 @@ class FeatureSelection(BaseEstimator, TransformerMixin):
         # find numerical variables or check variables entered by user
         self.variables = _find_numerical_variables(X, self.variables)
 
-        # train model with all features and cross-validation
-        model = cross_validate(
-            self.estimator,
-            X,
-            y,
-            cv=self.cv,
-            return_estimator=True,
-            scoring=self.scoring,
-        )
-
-        # store initial model performance
-        self.initial_model_performance_ = model["test_score"].mean()
-
         # list to collect selected features
         self.selected_features_ = []
 
-        # get performance metric
-        #scorer = get_scorer(self.scoring)
-
-        #What should I do after cross_validation is ejecuted?
-        #select features
+        # train model with all features and cross-validation
         for feature in self.variables:
+            model = cross_validate(
+                self.estimator,
+                X[feature].to_frame(),
+                y,
+                cv=self.cv,
+                return_estimator=True,
+                scoring=self.scoring,
+            )
 
-            self.selected_features_.append(feature)
+            if model["test_score"].mean() > self.threshold:
+                self.selected_features_.append(feature)
 
         self.input_shape_ = X.shape
 
