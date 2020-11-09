a2b88ec7d0db2988ebb2c968c82eefbf6f35ad70
SunnyxBd <bdriss.sana@gmail.com>
2020-11-09 14:02:42 +0100

    changed selector files name

--- feature_engine/selection/__init__.py
+++ feature_engine/selection/__init__.py
@@ -1,11 +1,11 @@
 """
 The module selection includes classes to select features or remove unwanted features.
 """
-from .eliminate_feature_by_feature_importance import RecursiveFeatureElimination
 from .drop_features import DropFeatures
 from .drop_constant_features import DropConstantFeatures
 from .drop_duplicate_features import DropDuplicateFeatures
 from .drop_correlated_features import DropCorrelatedFeatures
+from .recursive_feature_elimination import RecursiveFeatureElimination
 from .shuffle_features import ShuffleFeaturesSelector
 
 __all__ = [
