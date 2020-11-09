01a1c0aa4b03f92b7d01d3f9ee9853e0a4360fbf
SunnyxBd <bdriss.sana@gmail.com>
2020-11-09 14:19:18 +0100

    reordered feature_engine imports alphabetically

--- feature_engine/selection/recursive_feature_elimination.py
+++ feature_engine/selection/recursive_feature_elimination.py
@@ -4,11 +4,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.utils.validation import check_is_fitted
 
-from feature_engine.selection.base_selector import get_feature_importances
 from feature_engine.dataframe_checks import (
     _is_dataframe,
     _check_input_matches_training_df,
 )
+from feature_engine.selection.base_selector import get_feature_importances
 from feature_engine.variable_manipulation import (
     _define_variables,
     _find_numerical_variables,
