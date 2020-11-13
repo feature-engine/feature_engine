5609f7b485ee056b5b7b98692c8bb62682aaf78c
Soledad Galli <solegalli@protonmail.com>
2020-11-13 19:52:10 +0100

    Update test_single_feature_performance_selection.py

--- tests/test_selection/test_single_feature_performance_selection.py
+++ tests/test_selection/test_single_feature_performance_selection.py
@@ -9,34 +9,6 @@ from sklearn.tree import DecisionTreeRegressor
 from feature_engine.selection import SelectBySingleFeaturePerformance
 
 
-@pytest.fixture(scope="module")
-def df_test():
-    X, y = make_classification(
-        n_samples=1000,
-        n_features=12,
-        n_redundant=4,
-        n_clusters_per_class=1,
-        weights=[0.50],
-        class_sep=2,
-        random_state=1,
-    )
-
-    # trasform arrays into pandas df and series
-    colnames = ["var_" + str(i) for i in range(12)]
-    X = pd.DataFrame(X, columns=colnames)
-    y = pd.Series(y)
-    return X, y
-
-
-@pytest.fixture(scope="module")
-def load_diabetes_dataset():
-    # Load the diabetes dataset from sklearn
-    diabetes_X, diabetes_y = load_diabetes(return_X_y=True)
-    X = pd.DataFrame(diabetes_X)
-    y = pd.DataFrame(diabetes_y)
-    return X, y
-
-
 def test_default_parameters(df_test):
     X, y = df_test
     sel = SelectBySingleFeaturePerformance(RandomForestClassifier(random_state=1))
@@ -46,7 +18,6 @@ def test_default_parameters(df_test):
     Xtransformed = X.copy()
     Xtransformed.drop("var_3", 1, inplace=True)
     Xtransformed.drop("var_10", 1, inplace=True)
-
 
     # test init params
     assert sel.variables == [
@@ -79,7 +50,6 @@ def test_default_parameters(df_test):
         "var_9",
         "var_11",
     ]
-
     assert sel.feature_performance_ == {
         "var_0": 0.5957642619540211,
         "var_1": 0.5365534287221033,
@@ -94,8 +64,6 @@ def test_default_parameters(df_test):
         "var_10": 0.47404183834906727,
         "var_11": 0.5227164067525513,
     }
-
-
     # test transform output
     pd.testing.assert_frame_equal(sel.transform(X), Xtransformed)
 
@@ -103,11 +71,9 @@ def test_default_parameters(df_test):
 def test_regression_cv_3_and_r2(load_diabetes_dataset):
     #  test for regression using cv=3, and the r2 as metric.
     X, y = load_diabetes_dataset
-
     sel = SelectBySingleFeaturePerformance(
         estimator=LinearRegression(), scoring="r2", cv=3, threshold=0.01
     )
-
     sel.fit(X, y)
 
     # expected output
@@ -126,7 +92,6 @@ def test_regression_cv_3_and_r2(load_diabetes_dataset):
         9: 0.139,
     }
 
-
     # test init params
     assert sel.cv == 3
     assert sel.variables == list(X.columns)
@@ -138,7 +103,6 @@ def test_regression_cv_3_and_r2(load_diabetes_dataset):
         np.round(sel.feature_performance_[f], 3) == performance_dict[f]
         for f in sel.feature_performance_.keys()
     )
-
     # test transform output
     pd.testing.assert_frame_equal(sel.transform(X), Xtransformed)
 
@@ -148,7 +112,6 @@ def test_regression_cv_2_and_mse(load_diabetes_dataset):
     # add suitable threshold for regression mse
 
     X, y = load_diabetes_dataset
-    
     sel = SelectBySingleFeaturePerformance(
         estimator=DecisionTreeRegressor(random_state=0),
         scoring="neg_mean_squared_error",
@@ -160,7 +123,6 @@ def test_regression_cv_2_and_mse(load_diabetes_dataset):
 
     # expected output
     Xtransformed = X.copy()
-    
     Xtransformed.drop(
         Xtransformed.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]], axis=1, inplace=True
     )
@@ -172,7 +134,6 @@ def test_regression_cv_2_and_mse(load_diabetes_dataset):
     assert sel.threshold == 10
     # fit params
     assert sel.selected_features_ == []
-    
     assert sel.feature_performance_ == {
         0: -7657.154138192973,
         1: -5966.662211695372,
@@ -185,7 +146,6 @@ def test_regression_cv_2_and_mse(load_diabetes_dataset):
         8: -7641.414795123177,
         9: -6287.557824391035,
     }
-
     # test transform output
     print(sel.transform(X))
     pd.testing.assert_frame_equal(sel.transform(X), Xtransformed)
