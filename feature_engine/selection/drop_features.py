46e18917419257dd9c4d39d4ea809ff3825ba111
Soledad Galli <solegalli@protonmail.com>
2020-11-22 13:28:54 +0100

    add typehint, make argument compulsory, add test dropfeatures

--- feature_engine/selection/drop_features.py
+++ feature_engine/selection/drop_features.py
@@ -1,3 +1,6 @@
+from typing import List, Union
+import pandas as pd
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 
@@ -5,7 +8,6 @@ from feature_engine.dataframe_checks import (
     _is_dataframe,
     _check_input_matches_training_df,
 )
-from feature_engine.variable_manipulation import _check_input_parameter_variables
 
 
 class DropFeatures(BaseEstimator, TransformerMixin):
@@ -21,19 +23,20 @@ class DropFeatures(BaseEstimator, TransformerMixin):
 
     """
 
-    def __init__(self, features_to_drop=None):
+    def __init__(self, features_to_drop: List[Union[str, int]]):
 
-        self.features_to_drop = _check_input_parameter_variables(features_to_drop)
-
-        if len(self.features_to_drop) == 0:
+        if not isinstance(features_to_drop, list) or len(features_to_drop) == 0:
             raise ValueError(
-                "List of features to drop cannot be empty. Please pass at least 1 "
-                "variable to drop."
+                "features_to_drop should be a list with the name of the variables"
+                "you wish to drop from the dataframe."
             )
 
-    def fit(self, X, y=None):
+        self.features_to_drop = features_to_drop
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
         """
-        Verifies that the input X is a pandas dataframe
+        Verifies that the input X is a pandas dataframe, and that the variables to
+        drop exist in the training dataframe
 
         Parameters
         ----------
@@ -48,15 +51,11 @@ class DropFeatures(BaseEstimator, TransformerMixin):
         # check input dataframe
         X = _is_dataframe(X)
 
-        # check for non existent columns
-        non_existent = [x for x in self.features_to_drop if x not in X.columns]
-        if non_existent:
-            raise KeyError(
-                f"Columns '{', '.join(non_existent)}' not present in input dataframe, "
-                f"please check the columns and enter a new list of features to drop"
-            )
+        # X[self.features_to_drops] calls to pandas to check if columns are
+        # present in the df.
+        X[self.features_to_drop]
 
-        # check that user does not drop all columns returning empty dataframe
+        # check user is not removing all columns in the dataframe
         if len(self.features_to_drop) == len(X.columns):
             raise ValueError(
                 "The resulting dataframe will have no columns after dropping all "
@@ -68,7 +67,7 @@ class DropFeatures(BaseEstimator, TransformerMixin):
 
         return self
 
-    def transform(self, X):
+    def transform(self, X: pd.DataFrame):
         """
         Drops the variable or list of variables indicated by the user from the original
         dataframe and returns a new dataframe with the remaining subset of variables.
--- tests/test_selection/test_drop_features.py
+++ tests/test_selection/test_drop_features.py
@@ -19,6 +19,8 @@ def test_drop_2_variables(df_vartypes):
 
     # init params
     assert transformer.features_to_drop == ["City", "dob"]
+    # fit attr
+    assert transformer.input_shape_ == (4, 5)
     # transform params
     assert X.shape == (4, 3)
     assert type(X) == pd.DataFrame
@@ -28,7 +30,7 @@ def test_drop_2_variables(df_vartypes):
 def test_error_if_non_existing_variables(df_vartypes):
     # test case 2: passing variables that doesn't exist
     with pytest.raises(KeyError):
-        transformer = DropFeatures(features_to_drop="last_name")
+        transformer = DropFeatures(features_to_drop=["last_name"])
         transformer.fit_transform(df_vartypes)
 
 
@@ -51,3 +53,24 @@ def test_error_if_empty_list(df_vartypes):
     with pytest.raises(ValueError):
         transformer = DropFeatures(features_to_drop=[])
         transformer.fit_transform(df_vartypes)
+
+
+def test_drop_2_variables_integer_colnames(df_numeric_columns):
+    transformer = DropFeatures(features_to_drop=[0, 1])
+    X = transformer.fit_transform(df_numeric_columns)
+
+    # expected result
+    df = pd.DataFrame(
+        {
+            2: [20, 21, 19, 18],
+            3: [0.9, 0.8, 0.7, 0.6],
+            4: pd.date_range("2020-02-24", periods=4, freq="T"),
+        }
+    )
+
+    # init params
+    assert transformer.features_to_drop == [0, 1]
+    # fit attr
+    assert transformer.input_shape_ == (4, 5)
+    # transform params
+    pd.testing.assert_frame_equal(X, df)
