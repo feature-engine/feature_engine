1281f5cad8f72b34d568a4007611040aa6d75272
Soledad Galli <solegalli@protonmail.com>
2021-01-11 09:45:53 -0300

    fix bug, reword docstrings, update notebook

--- feature_engine/imputation/drop_missing_data.py
+++ feature_engine/imputation/drop_missing_data.py
@@ -12,25 +12,21 @@ from feature_engine.variable_manipulation import _check_input_parameter_variable
 
 class DropMissingData(BaseImputer):
     """
-    The DropMissingData() will delete rows containing NA values.
+    DropMissingData() will delete rows containing missing values. The DropMissingData()
+    works for both numerical and categorical variables. DropMissingData can
+    automatically select all the variables, or alternatively, all the variables with
+    missing data in the train set. Then the observations with NA will be dropped for
+    these variable groups.
 
-    DropMissingData() will delete rows containing NA values from the variables
-    indicated by the user, or variables with NA values in the train set.
-
-    The DropMissingData() works for both numerical and categorical variables.
-    The user can pass a list with the variables rows containing with NA should be dropped. Alternatively, the imputer will select variables with NA values
-    from the training set.
+    The user has the option to indicate for which variables the observations with NA
+    should be removed.
 
     Parameters
     ----------
-    missing_only : bool, defatult=True
-        Indicates if rows with NA  should be dropped to variables with missing
-        data or to all variables.
-
-        True: rows with NA will be dropped only for those variables that showed
-        missing data during fit.
-
-        False: rows with NA will be dropped for all variables
+    missing_only : bool, default=True
+        If true, missing observations will be dropped only for the variables that were
+        seen to have NA in the train set, during fit. If False, observations with NA
+        will be dropped from all variables.
 
     variables : list, default=None
         The list of variables to be imputed. If None, the imputer will find and
@@ -38,8 +34,8 @@ class DropMissingData(BaseImputer):
 
     **Note**
     The transformer will first select all variables or all user entered
-    variables and if how=missing_only, it will re-select from the original group
-    only those that show missing data in during fit.
+    variables and if `missing_only=True`, it will re-select from the original group
+    only those that show missing data in during fit, that is in the train set.
 
     Attributes
     ----------
@@ -51,17 +47,17 @@ class DropMissingData(BaseImputer):
     fit:
         Learn the variables for which the rows with NA will be deleted
     transform:
-        Add the missing indicators.
+        Remove observations with NA
     fit_transform:
-        Fit to the data, then trasnform it.
+        Fit to the data, then transform it.
     return_dropped_data:
-        Returns the dataframe with rows containing NA values.
+        Returns the dataframe with the rows that contain NA .
     """
 
     def __init__(
-        self,
-        missing_only: bool = True,
-        variables: Union[None, int, str, List[Union[str, int]]] = None,
+            self,
+            missing_only: bool = True,
+            variables: Union[None, int, str, List[Union[str, int]]] = None,
     ) -> None:
 
         if not isinstance(missing_only, bool):
@@ -69,32 +65,6 @@ class DropMissingData(BaseImputer):
 
         self.variables = _check_input_parameter_variables(variables)
         self.missing_only = missing_only
-
-    def return_dropped_data(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Returns the subset of the dataframe which contains rows with NA values.
-
-        Parameters
-        ----------
-        X : pandas dataframe of shape = [n_samples, n_features]
-            The dataset to from which rows containing NA should be dropped.
-
-        Raises
-        ------
-        TypeError
-            If the input is not a Pandas DataFrame
-
-        Returns
-        -------
-        X : pandas dataframe.
-            The cleaned version of dataset that does not contain rows with NA values.
-        """
-
-        X = self._check_transform_input_and_state(X)
-
-        idx = pd.isnull(X[self.variables_]).any(1)
-        idx = idx[idx == True]
-        return X.loc[idx.index, :]
 
     def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None):
         """
@@ -144,7 +114,7 @@ class DropMissingData(BaseImputer):
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
-        Remove rows with NA values.
+        Remove rows with missing values.
 
         Parameters
         ----------
@@ -153,12 +123,41 @@ class DropMissingData(BaseImputer):
 
         Returns
         -------
-        X_transformed : pandas dataframe of shape = [n_samples, n_features]
-            The dataframe containing rows with no NA values.
+        X_transformed : pandas dataframe
+            The complete case dataframe for the selected variables, of shape
+            [n_samples - rows_with_na, n_features]
         """
 
         X = self._check_transform_input_and_state(X)
 
-        X.dropna(axis=0, how="any", subset=self.variables, inplace=True)
+        X.dropna(axis=0, how="any", subset=self.variables_, inplace=True)
 
         return X
+
+    def return_na_data(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Returns the subset of the dataframe which contains the rows with missing values.
+        This method could be useful in production, in case we want to store the
+        observations that will not be fed into the model.
+
+        Parameters
+        ----------
+        X : pandas dataframe of shape = [n_samples, n_features]
+            The dataset to from which rows containing NA should be retained.
+
+        Raises
+        ------
+        TypeError
+            If the input is not a Pandas DataFrame
+
+        Returns
+        -------
+        X : pandas dataframe of shape = [obs_with_na, features]
+            The cdataframe portion that contains only the rows with missing values.
+        """
+
+        X = self._check_transform_input_and_state(X)
+
+        idx = pd.isnull(X[self.variables_]).any(1)
+        idx = idx[idx]
+        return X.loc[idx.index, :]
