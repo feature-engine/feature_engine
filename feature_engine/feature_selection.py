db42c058dae55a9abc18ca4b563d134f10917ce7
Thibault Blanc <thibaultbl@users.noreply.github.com>
2021-05-27 16:44:35 +0200

    Update feature_selection.py
    Added pandas import

--- feature_engine/feature_selection.py
+++ feature_engine/feature_selection.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 from feature_engine.dataframe_checks import _is_dataframe, _check_input_matches_training_df
