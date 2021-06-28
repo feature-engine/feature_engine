1ef270522264a138a2134233d045176f2d93d536
Soledad Galli <solegalli@protonmail.com>
2021-06-28 17:40:34 +0200

    reorders import in outlier init

--- feature_engine/outliers/__init__.py
+++ feature_engine/outliers/__init__.py
@@ -3,7 +3,7 @@ The module outliers includes classes to remove or cap outliers.
 """
 
 from .artbitrary import ArbitraryOutlierCapper
-from .trimmer import OutlierTrimmer
 from .winsorizer import Winsorizer
+from .trimmer import OutlierTrimmer
 
 __all__ = ["Winsorizer", "ArbitraryOutlierCapper", "OutlierTrimmer"]
