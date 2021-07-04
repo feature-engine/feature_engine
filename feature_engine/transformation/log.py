117ea3061ec9cf65f9d012aff4875d2b88e8cf71
Soledad Galli <solegalli@protonmail.com>
2021-07-04 11:31:13 +0200

    add inverse_transform to transformers from transformation module (#283)
    * New inverse methods in transformers
    
    * Log inverse test case
    
    * More unit tests + reciprocal error fixed in return
    
    * #248 changes requested after initial PR
    
    * polish PR on inverse_transform
    
    * reorders import in outlier init
    
    * adds inverse_transform to jupyter demos
    
    * make minor changes to log docstrings
    
    Co-authored-by: NicoGalli <nicolas.galli@yahoo.com>

--- feature_engine/transformation/log.py
+++ feature_engine/transformation/log.py
@@ -14,10 +14,10 @@ from feature_engine.variable_manipulation import _check_input_parameter_variable
 class LogTransformer(BaseNumericalTransformer):
     """
     The LogTransformer() applies the natural logarithm or the base 10 logarithm to
-    numerical variables. The natural logarithm is logarithm in base e.
+    numerical variables. The natural logarithm is the logarithm in base e.
 
-    The LogTransformer() only works with numerical non-negative values. If the variable
-    contains a zero or a negative value, the transformer will return an error.
+    The LogTransformer() only works with positive values. If the variable
+    contains a zero or a negative value the transformer will return an error.
 
     A list of variables can be passed as an argument. Alternatively, the transformer
     will automatically select and transform all variables of type numeric.
@@ -45,9 +45,11 @@ class LogTransformer(BaseNumericalTransformer):
     fit:
         This transformer does not learn parameters.
     transform:
-        Transforms the variables using log transformation.
+        Transform the variables using the logarithm.
     fit_transform:
         Fit to data, then transform it.
+    inverse_transform:
+        Convert the data back to the original representation.
     """
 
     def __init__(
@@ -66,9 +68,9 @@ class LogTransformer(BaseNumericalTransformer):
         """
         This transformer does not learn parameters.
 
-        Select the numerical variables and determines whether the logarithm
-        can be applied on the selected variables (it checks if the variables
-        are all positive).
+        Selects the numerical variables and determines whether the logarithm
+        can be applied on the selected variables, i.e., it checks that the variables
+        are positive.
 
         Parameters
         ----------
@@ -109,7 +111,7 @@ class LogTransformer(BaseNumericalTransformer):
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
-        Transforms the variables using log transformation.
+        Transform the variables using log transformation.
 
         Parameters
         ----------
@@ -145,6 +147,41 @@ class LogTransformer(BaseNumericalTransformer):
             X.loc[:, self.variables_] = np.log(X.loc[:, self.variables_])
         elif self.base == "10":
             X.loc[:, self.variables_] = np.log10(X.loc[:, self.variables_])
+
+        return X
+
+    def inverse_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Convert the data back to the original representation.
+
+        Parameters
+        ----------
+        X: Pandas DataFrame of shape = [n_samples, n_features]
+            The data to be transformed.
+
+        Raises
+        ------
+        TypeError
+            If the input is not a Pandas DataFrame
+        ValueError
+            - If the variable(s) contain null values
+            - If the df has different number of features than the df used in fit()
+            - If some variables contains zero or negative values
+
+        Returns
+        -------
+        X: pandas dataframe
+            The dataframe with the transformed variables.
+        """
+
+        # check input dataframe and if class was fitted
+        X = super().transform(X)
+
+        # inverse_transform
+        if self.base == "e":
+            X.loc[:, self.variables_] = np.exp(X.loc[:, self.variables_])
+        elif self.base == "10":
+            X.loc[:, self.variables_] = np.array(10 ** X.loc[:, self.variables_])
 
         return X
 
