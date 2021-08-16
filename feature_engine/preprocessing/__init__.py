47affc31fd9efab26814339f29a7c3186dd7c42d
thibault <thibault.blanc@yahoo.fr>
2021-08-16 22:31:53 +0200

    Renamed similar_columns as match_columns

--- feature_engine/preprocessing/__init__.py
+++ feature_engine/preprocessing/__init__.py
@@ -2,7 +2,7 @@
 The module preprocessing includes classes to check that train and tests are similar.
 """
 
-from .similar_columns import SimilarColumns
+from .match_columns import SimilarColumns
 
 __all__ = [
     "SimilarColumns",
