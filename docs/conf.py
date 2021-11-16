83bf799298312fa591b62c3ee4cc35dc8181bc49
Soledad Galli <solegalli@protonmail.com>
2021-11-16 15:19:00 +0100

    re-inserts paths to doc config file

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,9 @@ from datetime import datetime
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "feature_engine")
 sys.path.insert(0, os.path.abspath("sphinxext"))
 from github_link import make_linkcode_resolve
 
