0c5f6c9cc01f3be2e6d8792cc1aef81683d4d9ad
Morgan-Sell <morganpsell@gmail.com>
2021-11-30 17:11:06 -0800

    identifies column names that have NaN values. Informs user via a warning. Need to create test.

--- feature_engine/encoding/base_encoder.py
+++ feature_engine/encoding/base_encoder.py
@@ -144,14 +144,22 @@ class BaseCategoricalTransformer(BaseEstimator, TransformerMixin):
 
         # check if NaN values were introduced by the encoding
         if X[self.encoder_dict_.keys()].isnull().sum().sum() > 0:
+            nan_columns = X.columns[X.isnull().any()].tolist()
+            if len(nan_columns) > 1:
+                nan_columns_str = ", ".join(nan_columns)
+            else:
+                nan_columns_str = nan_columns[0]
+
             warnings.warn(
-                "NaN values were introduced in the returned dataframe by the encoder."
+                f"NaN values were detected in the feature(s) titled {nan_column_str}."
                 "This means that some of the categories in the input dataframe were "
                 "not present in the training set used when the fit method was called. "
                 "Thus, mappings for those categories do not exist. Try using the "
                 "RareLabelCategoricalEncoder to remove infrequent categories before "
                 "calling this encoder."
             )
+
+
 
         return X
 
