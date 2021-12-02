784fdd8335eb9b1cab4b3dbbd07c5bda884f12a9
dodoarg <eargiolas96@gmail.com>
2021-12-02 20:44:40 +0100

    removed irrelevant file

--- feature_engine/parameter_checks.py
+++ feature_engine/parameter_checks.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional
+from typing import Optional
 
 
 def _define_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
@@ -34,41 +34,3 @@ def _define_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
         raise TypeError("The parameter can only take a dictionary or None")
 
     return dict_
-
-
-def _parse_features_to_extract(feats_requested, feats_supported: List[str]) -> Any:
-    """
-    Parses argument features_to_extract of ExtractDateTransformer
-    (potentially shares utility with other similar feature
-    extractor transformers)
-
-    Parameters
-    ----------
-    feats_requested
-        argument passed to the transformer as features_to_extract
-
-    feats_supported: list[str]
-        list of supported date features that the transformer is able
-        to extract from the existing datetime variables
-    """
-    if not isinstance(feats_requested, (str, list)):
-        raise TypeError("feats_requested must be either a str or a list of str")
-
-    if not isinstance(feats_supported, list) or any(
-        not isinstance(feat, str) for feat in feats_supported
-    ):
-        raise TypeError("feats_supported must be a list of str")
-
-    if feats_requested == "all":
-        return feats_supported
-
-    if isinstance(feats_requested, str):
-        feats_requested = [feats_requested]
-
-    if any(feature not in feats_supported for feature in feats_requested):
-        raise ValueError(
-            "At least one of the requested feature is not supported. "
-            "Supported features are {}.".format(", ".join(feats_supported))
-        )
-
-    return feats_requested
