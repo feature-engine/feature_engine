69589198ade46534d2028754591932779aa9e978
Morgan-Sell <morganpsell@gmail.com>
2021-12-24 11:13:21 -0800

    add rare_labels param to test_warn_if_transform_df_contains_categories_not_seen_in_fit for OrdinalEncoder, PRatioEncoder, and WoEEncoder

--- tests/test_encoding/test_woe_encoder.py
+++ tests/test_encoding/test_woe_encoder.py
@@ -92,8 +92,9 @@ def test_warn_if_transform_df_contains_categories_not_seen_in_fit(df_enc, df_enc
     # in training dataset
     msg = "During the encoding, NaN values were introduced in the feature(s) var_A."
 
-    encoder = WoEEncoder(variables=None)
+    # check for error when rare_labels equals 'raise'
     with pytest.warns(UserWarning) as record:
+        encoder = WoEEncoder(rare_labels="ignore")
         encoder.fit(df_enc[["var_A", "var_B"]], df_enc["target"])
         encoder.transform(df_enc_rare[["var_A", "var_B"]])
 
@@ -102,6 +103,16 @@ def test_warn_if_transform_df_contains_categories_not_seen_in_fit(df_enc, df_enc
     # check that the message matches
     assert record[0].message.args[0] == msg
 
+    # check for error when rare_labels equals 'raise'
+    with pytest.raises(ValueError) as record:
+        encoder = WoEEncoder(rare_labels="raise")
+        encoder.fit(df_enc)
+        encoder.transform(df_enc_rare)
+
+    # check that only one warning was raised
+    assert len(record) == 1
+    # check that the message matches
+    assert record[0].message.args[0] == msg
 
 def test_error_if_target_not_binary():
     # test case 4: the target is not binary
