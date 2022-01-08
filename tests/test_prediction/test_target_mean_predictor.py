743ef44b7869df6ea651e2698d4eeefb34336eb4
Morgan-Sell <morganpsell@gmail.com>
2022-01-07 17:02:56 -0800

    start creating first check

--- tests/test_prediction/test_target_mean_predictor.py
+++ tests/test_prediction/test_target_mean_predictor.py
@@ -3,3 +3,10 @@ import numpy
 
 from feature_engine.prediction import TargetMeanPredictor
 
+
+def test_target_mean_predictor_fit(df_enc_category_dtypes):
+    predictor = TargetMeanPredictor()
+    predictor.fit(df_enc_category_dtypes[["var_A", "var_B"]], df_enc_category_dtypes["target"])
+
+    # test init params
+
