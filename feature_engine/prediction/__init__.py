2295a27d3e73e27a4c1c36f9f3ffc75e31c557a3
Morgan-Sell <morganpsell@gmail.com>
2022-01-07 17:22:46 -0800

    create prediction init file and expand test_target_mean_predictor_fit()

---
+++ feature_engine/prediction/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module includes a predictor that returns the mean for discretised and encoded variables.
+"""
+
+from .target_mean_prediction import TargetMeanPredictor
+
+__all__ = [
+    "TargetMeanPredictor"
+]
--- tests/test_prediction/test_target_mean_predictor.py
+++ tests/test_prediction/test_target_mean_predictor.py
@@ -4,9 +4,16 @@ import numpy
 from feature_engine.prediction import TargetMeanPredictor
 
 
-def test_target_mean_predictor_fit(df_enc_category_dtypes):
-    predictor = TargetMeanPredictor()
-    predictor.fit(df_enc_category_dtypes[["var_A", "var_B"]], df_enc_category_dtypes["target"])
+def test_target_mean_predictor_fit(df_pred):
+    predictor = TargetMeanPredictor(
+        variables=["City", "Studies", "Age"],
+        bins=5,
+        strategy="equal-width"
+    )
+    predictor.fit(df_pred, df_pred["target"])
 
     # test init params
+    assert predictor.variables == ["City", "Studies", "Age"]
+    assert predictor.bins == 5
+    assert predictor.strategy == "equal-width"
 
