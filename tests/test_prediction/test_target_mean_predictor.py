67adf9ddfffad5b99ca9a0271722a3da02eb56c4
Morgan-Sell <morganpsell@gmail.com>
2022-01-08 12:47:52 -0800

    bug: KeyError:None when slicing df_pred even though all variables exists w/in df. sucessfully printed sliced df using column names

--- tests/test_prediction/test_target_mean_predictor.py
+++ tests/test_prediction/test_target_mean_predictor.py
@@ -6,14 +6,15 @@ from feature_engine.prediction import TargetMeanPredictor
 
 def test_target_mean_predictor_fit(df_pred):
     predictor = TargetMeanPredictor(
-        variables=["City", "Studies", "Age"],
+        variables=None,
         bins=5,
         strategy="equal-width"
     )
-    predictor.fit(df_pred, df_pred["Marks"])
+    print(df_pred[["City", "Studies", "Age"]].head())
+    predictor.fit(df_pred[["City", "Studies", "Age"]], df_pred["Marks"])
 
     # test init params
-    assert predictor.variables == ["City", "Studies", "Age"]
+    assert predictor.variables is None
     assert predictor.bins == 5
     assert predictor.strategy == "equal-width"
 
