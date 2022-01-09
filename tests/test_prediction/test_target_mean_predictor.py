4c9beeafcb7028aafa24da32d8dc82f3755e522f
Morgan-Sell <morganpsell@gmail.com>
2022-01-09 14:49:04 -0800

    add fit params tests

--- tests/test_prediction/test_target_mean_predictor.py
+++ tests/test_prediction/test_target_mean_predictor.py
@@ -10,11 +10,14 @@ def test_target_mean_predictor_fit(df_pred):
         bins=5,
         strategy="equal-width"
     )
-    print(df_pred[["City", "Studies", "Age"]].head())
-    predictor.fit(df_pred[["City", "Studies", "Age"]], df_pred["Marks"])
+
+    predictor.fit(df_pred[["City", "Age"]], df_pred["Marks"])
 
     # test init params
     assert predictor.variables is None
     assert predictor.bins == 5
     assert predictor.strategy == "equal-width"
+    # test fit params
+    assert predictor.variables_ == ["City", "Age"]
+    assert predictor.disc_mean_dict_ == {"Age": {0: 0.8, 1: 0.3, 2: 0.5, 3: 0.8, 4: 0.25}}
 
