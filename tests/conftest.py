076a63ad1c2a3297af57e2376ec5cd207b3efbc5
Morgan-Sell <morganpsell@gmail.com>
2022-01-12 16:17:35 -0800

    create conftest_prediction and move df_pred() from conftest to conftest_prediction

--- tests/conftest.py
+++ tests/conftest.py
@@ -216,38 +216,3 @@ def df_normal_dist():
 
     return df
 
-
-@pytest.fixture(scope="module")
-def df_pred():
-    data = {
-        "Name": ["tom", "nick", "krish", "megan", "peter", "jordan", "fred", "sam", "alexa", "brittany"],
-        "City": [
-            "London",
-            "Manchester",
-            "Liverpool",
-            "Bristol",
-            "Manchester",
-            "Liverpool",
-            "London",
-            "Liverpool",
-            "Manchester",
-            "London"],
-        "Studies": [
-            "Bachelor",
-            "Bachelor",
-            "PhD",
-            "Masters",
-            "Bachelor",
-            "PhD",
-            "None",
-            "Masters",
-            "Masters",
-            "Bachelor"
-        ],
-        "Age": [20, 44, 19, 33, 51, 40, 41, 37, 30, 54],
-        "Marks": [1.0, 0.8, 0.6, 0.1, 0.3, 0.4, 0.8, 0.6, 0.5, 0.2],
-    }
-
-    df = pd.DataFrame(data)
-
-    return df
