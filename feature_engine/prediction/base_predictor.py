b5f35ae1628a15fe66c1937c4b4ed6ad6a893584
Morgan-Sell <morganpsell@gmail.com>
2022-01-17 11:29:28 -0800

    add fit() and supporting methods to BaseTargetMeanPredictor class

--- feature_engine/prediction/base_predictor.py
+++ feature_engine/prediction/base_predictor.py
@@ -74,3 +74,102 @@ class BaseTargetMeanPredictor(BaseEstimator):
         self.variables = _check_input_parameter_variables(variables)
         self.bins = bins
         self.strategy = strategy
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None) -> pd.DataFrame:
+        """
+        Fit predictor per variables.
+
+        Parameters
+        ----------
+        X : pandas dataframe of shape = [n_samples, n_features]
+            The training input samples.
+
+        y : pandas series of shape = [n_samples,]
+            The target variable.
+        """
+        # check if 'X' is a dataframe
+        _is_dataframe(X)
+
+        # check variables
+        self.variables_ = _find_all_variables(X, self.variables)
+
+        # identify categorical and numerical variables
+        self.variables_categorical_ = list(
+            X[self.variables_].select_dtypes(include="object").columns
+        )
+        self.variables_numerical_ = list(
+            X[self.variables_].select_dtypes(include="number").columns
+        )
+
+        # encode categorical variables and discretise numerical variables
+        if self.variables_categorical_ and self.variables_numerical_:
+            pipeline = self._make_combined_pipeline()
+
+        elif self.variables_categorical_:
+            pipeline = self._make_categorical_pipeline()
+
+        else:
+            pipeline = self._make_numerical_pipeline()
+
+        self.pipeline = pipeline
+        self.pipeline.fit(X, y)
+
+        self.n_features_in_ = X.shape[1]
+
+        return self
+
+    def _make_numerical_pipeline(self):
+        """
+        Create pipeline for a dataframe solely comprised of numerical variables
+        using a discretiser and encoder.
+        """
+        encoder = MeanEncoder(variables=self.variables_numerical_, errors="raise")
+
+        _pipeline_numerical = Pipeline(
+            [
+                ("discretisation", self._make_discretiser()),
+                ("encoder", encoder),
+            ]
+        )
+
+        return _pipeline_numerical
+
+    def _make_categorical_pipeline(self):
+        """
+        Instantiate the encoder for a dataframe solely comprised of categorical
+        variables.
+        """
+
+        return MeanEncoder(
+            variables=self.variables_categorical_, errors="raise"
+        )
+
+    def _make_combined_pipeline(self):
+
+        encoder_num = MeanEncoder(variables=self.variables_numerical_, errors="raise")
+        encoder_cat = MeanEncoder(variables=self.variables_categorical_, errors="raise")
+
+        _pipeline_combined = Pipeline(
+            [
+                ("discretisation", self._make_discretiser()),
+                ("encoder_num", encoder_num),
+                ("encoder_cat", encoder_cat),
+            ]
+        )
+
+        return _pipeline_combined
+
+    def _make_discretiser(self):
+        """
+        Instantiate either EqualWidthDiscretiser or EqualFrequencyDiscretiser.
+        """
+        if self.strategy == "equal_width":
+            discretiser = EqualWidthDiscretiser(
+                bins=self.bins, variables=self.variables_numerical_, return_object=True
+            )
+        else:
+            discretiser = EqualFrequencyDiscretiser(
+                q=self.bins, variables=self.variables_numerical_, return_object=True
+            )
+
+        return discretiser
