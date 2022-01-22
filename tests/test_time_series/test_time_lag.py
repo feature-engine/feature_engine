d02ec3cfe0a2332f4c15e94eeae5de847037fe4f
Morgan-Sell <morganpsell@gmail.com>
2022-01-22 11:55:27 -0800

    create test_time_lag_frequency_shift_and_ignore_original_data()

--- tests/test_time_series/test_time_lag.py
+++ tests/test_time_series/test_time_lag.py
@@ -4,7 +4,7 @@ import pytest
 from feature_engine.timeseries.forecasting import TimeSeriesLagTransformer
 
 
-def test_time_lag_period_shift(df_time):
+def test_time_lag_period_shift_and_keep_original_data(df_time):
     # Case 1: The lag is correctly performed using the 'period' param.
     transformer = TimeSeriesLagTransformer(
         variables=["ambient_temp", "module_temp"],
@@ -30,6 +30,34 @@ def test_time_lag_period_shift(df_time):
     expected_results_df = pd.DataFrame(
         data=expected_results,
         index=date_time
+    )
+
+    assert df_tr.head(5) == expected_results_df
+
+
+def test_time_lag_frequency_shift_and_ignore_original_data(df_time):
+    # Case 2: Data is properly transformed using the 'freq' param.
+    transformer = TimeSeriesLagTransformer(
+        freq="1h",
+        keep_original=False
+    )
+    df_tr = transformer.transform(df_time)
+
+    date_time = [
+        pd.Timestamp('2020-05-15 13:00:00'),
+        pd.Timestamp('2020-05-15 13:15:00'),
+        pd.Timestamp('2020-05-15 13:30:00'),
+        pd.Timestamp('2020-05-15 13:45:00'),
+        pd.Timestamp('2020-05-15 14:00:00')
+    ]
+    expected_results = {
+        "ambient_temp_lag_1h": [31.31, 31.51, 32.15, 32.39, 32.62],
+        "module_temp_lag_1h": [49.18, 49.84, 52.35, 50.63, 49.61],
+        "irradiation_lag_1h": [0.51, 0.79, 0.65, 0.76, 0.42],
+    }
+    expected_results_df = pd.DataFrame(
+        data=expected_results,
+        index=date_time,
     )
 
     assert df_tr.head(5) == expected_results_df
