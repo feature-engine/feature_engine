58b744681d2b5ae80d1205b2713183e0c3622150
Morgan-Sell <morganpsell@gmail.com>
2022-01-24 18:26:15 -0800

    create test_attributes_upon_fitting()

--- tests/test_prediction/test_target_mean_classifier.py
+++ tests/test_prediction/test_target_mean_classifier.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import pytest
 from sklearn.exceptions import NotFittedError
+from sklearn.pipeline import Pipeline
 
+from feature_engine.discretisation import (
+    EqualFrequencyDiscretiser,
+    EqualWidthDiscretiser,
+)
+from feature_engine.encoding import MeanEncoder
 from feature_engine.prediction import TargetMeanClassifier
 from tests.test_prediction.conftest import df_pred
 
@@ -33,3 +39,28 @@ def test_default_params():
     assert transformer.variables is None
     assert transformer.bins == 5
     assert transformer.strategy == "equal_width"
+
+
+def test_attributes_upon_fitting(df_pred):
+    transformer = TargetMeanClassifier(
+        variables=None,
+        bins=7,
+        strategy="equal_frequency"
+    )
+
+    transformer.fit(df_pred[["City", "Age"]], df_pred["Marks"])
+
+    # test init params
+    assert transformer.variables is None
+    assert transformer.bins == 7
+    assert transformer.strategy == "equal_frequency"
+    # test attributes
+    assert transformer.variables_categorical_ == ["City"]
+    assert transformer.variables_numerical_ == ["Age"]
+    assert transformer.classes_ == [1, 0]
+    assert transformer.pipeline_ == Pipeline(steps=[
+        ('discretiser', EqualFrequencyDiscretiser(
+            q=7, return_object=True, variables=['Age'])),
+        ('encoder_num', MeanEncoder(errors='raise', variables=['Age'])),
+        ('encoder_cat', MeanEncoder(errors='raise', variables=['City']))])
+    assert transformer.n_features_in_ == 2
