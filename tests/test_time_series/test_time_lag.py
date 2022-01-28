6b018ccdff2242ec26431543b35d2f50a4cd48dd
Morgan-Sell <morganpsell@gmail.com>
2022-01-27 19:12:28 -0800

    edit tests to match changes in class code base

--- tests/test_time_series/test_time_lag.py
+++ tests/test_time_series/test_time_lag.py
@@ -24,8 +24,8 @@ def test_time_lag_period_shift_and_keep_original_data(df_time):
     expected_results = {
         "ambient_temp": [31.31, 31.51, 32.15, 32.39, 32.62],
         "module_temp": [49.18, 49.84, 52.35, 50.63, 49.61],
-        "ambient_temp_lag_3pds": ['nan', 'nan', 'nan', 31.31, 31.51],
-        "module_temp_lag_3pds": ['nan', 'nan', 'nan', 49.18, 49.84],
+        "ambient_temp_lag_3": ['nan', 'nan', 'nan', 31.31, 31.51],
+        "module_temp_lag_3": ['nan', 'nan', 'nan', 49.18, 49.84],
     }
     expected_results_df = pd.DataFrame(
         data=expected_results,
@@ -81,9 +81,9 @@ def test_time_lag_fill_value(df_time):
         "ambient_temp": [31.31, 31.51, 32.15, 32.39, 32.62],
         "module_temp": [49.18, 49.84, 52.35, 50.63, 49.61],
         "irradiation": [0.51, 0.79, 0.65, 0.76, 0.42],
-        "ambient_temp_lag_2pds": ['nan', 'nan', 31.31, 31.51, 32.15],
-        "module_temp_lag_2pds": ['nan', 'nan', 49.18, 49.84, 52.35],
-        "irradiation_lag_2pds": ['nan', 'nan', 0.51, 0.79, 0.65],
+        "ambient_temp_lag_2": ['nan', 'nan', 31.31, 31.51, 32.15],
+        "module_temp_lag_2": ['nan', 'nan', 49.18, 49.84, 52.35],
+        "irradiation_lag_2": ['nan', 'nan', 0.51, 0.79, 0.65],
     }
     expected_results_df = pd.DataFrame(
         data=expected_results,
