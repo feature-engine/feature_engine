de1ed69db96ce8c47df098634f997b81421d7260
Morgan-Sell <morganpsell@gmail.com>
2022-02-03 16:55:35 -0800

    fix and extend test_raises_error_when_wrong_input_params()

--- tests/test_time_series/test_forecasting/test_lag_features.py
+++ tests/test_time_series/test_forecasting/test_lag_features.py
@@ -5,17 +5,25 @@ import pytest
 from feature_engine.timeseries.forecasting import LagFeatures
 
 _false_input_params = [
-    ([3, 2], "pizza", ["tango", "empanada"], 9),
-    ("mate", ["bombilla", "limpiado"], True, "cocinado"),
-    (True, False, "asado", [1984, 1999]),
+    ([3, 2], "pizza", ["tango", "empanada"], "tigre", 9),
+    ("mate", ["bombilla", "limpiado"], True, False, "cocinado"),
+    (True, -3, "asado", 4, [1984, 1999]),
+    ("alfajor", [3, -8, 10], ["jamon", "merienda"], ["mate", "bombilla"], 10),
+    ("asado", False, "empanada", None, ["tango", "empanada"]),
+]
+
+_test_types_init_params = [
+    ("empanada", 3, "1d", "raise", True),
+    (["che", "medialuna"], [2, 4, 6], ["1h", "30m", "4h"], "ignore", True),
+    (None, None, None, "raise", False),
 ]
 
 
 @pytest.mark.parametrize(
-    "_variables, _periods, _freq, _drop_original", _false_input_params
+    "_variables, _periods, _freq, _missing_values, _drop_original", _false_input_params
 )
 def test_raises_error_when_wrong_input_params(
-    _variables, _periods, _freq, _drop_original, df_time
+    _variables, _periods, _freq, _missing_values, _drop_original, df_time
 ):
     with pytest.raises(KeyError):
         transformer1 = LagFeatures(variables=_variables)
@@ -30,19 +38,17 @@ def test_raises_error_when_wrong_input_params(
         transformer3.fit(df_time)
         transformer3.transform(df_time)
     with pytest.raises(ValueError):
-        transformer4 = LagFeatures(drop_original=_drop_original)
+        transformer4 = LagFeatures(missing_values=_missing_values)
         transformer4.fit(df_time)
         transformer4.transform(df_time)
+    with pytest.raises(ValueError):
+        transformer5 = LagFeatures(drop_original=_drop_original)
+        transformer5.fit(df_time)
+        transformer5.transform(df_time)
 
-
-_test_init_params = [
-    ("empanada", 3, "1d", "raise", True),
-    (["che", "medialuna"], [2, 4, 6], ["1h", "30m", "4h"], "ignore", True),
-    (None, None, None, "raise", False),
-]
 
 @pytest.mark.parametrize(
-    "_variables, _periods, _freq, _missing_values, _drop_original", _test_init_params
+    "_variables, _periods, _freq, _missing_values, _drop_original", _test_types_init_params
 )
 def test_different_types_for_init_params(
         _variables, _periods, _freq, _missing_values, _drop_original
