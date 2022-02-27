d45b27d19055c37b90f0238679ea9db954924720
Morgan-Sell <morganpsell@gmail.com>
2022-02-26 18:58:01 -0800

    create test_correct_window_when_using_periods. Need to resolve assertion error. dataframes do not reconcile.

--- tests/test_time_series/test_forecasting/test_window_features.py
+++ tests/test_time_series/test_forecasting/test_window_features.py
@@ -52,6 +52,7 @@ def test_error_when_non_permitted_param_freq(_freqs):
     with pytest.raises(ValueError):
         WindowFeatures(periods=None, freq=_freqs)
 
+
 def test_get_feature_names_out(df_time):
     # input features
     input_features = ["ambient_temp", "module_temp", "irradiation"]
@@ -94,3 +95,35 @@ def test_get_feature_names_out(df_time):
     assert tr.get_feature_names_out(input_features=input_features) == output
     assert tr.get_feature_names_out(input_features=input_features[0:2]) == output[0:2]
     assert tr.get_feature_names_out(input_features=[input_features[0]]) == [output[0]]
+
+
+def test_correct_window_when_using_periods(df_time):
+    date_time = [
+        "2020-05-15 12:00:00",
+        "2020-05-15 12:15:00",
+        "2020-05-15 12:30:00",
+        "2020-05-15 12:45:00",
+        "2020-05-15 13:00:00",
+        "2020-05-15 13:15:00",
+        "2020-05-15 13:30:00",
+        "2020-05-15 13:45:00",
+        '2020-05-15 14:00:00',
+    ]
+
+    expected_results = {
+        "ambient_temp": [31.31, 31.51, 32.15, 32.39, 32.62, 32.5, 32.52, 32.68, 33.76],
+        "module_temp": [49.18, 49.84, 52.35, 50.63, 49.61, 47.01, 46.67, 47.52, 49.8],
+        "irradiation": [0.51, 0.79, 0.65, 0.76, 0.42, 0.49, 0.57, 0.56, 0.74],
+        "color": ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
+        "ambient_temp_window_3_periods_2": [np.nan, np.nan, np.nan, np.nan, 31.51, 32.15, 32.39, 32.5, 32.52],
+        "module_temp_window_3_periods_2": [np.nan, np.nan, np.nan, np.nan, 49.84, 50.63, 50.63, 49.61, 47.01],
+        "irradiation_window_3_periods_2": [np.nan, np.nan, np.nan, np.nan, 0.65, 0.76, 0.65, 0.49, 0.49],
+    }
+    expected_results_df = pd.DataFrame(data=expected_results, index=date_time)
+
+    transformer = WindowFeatures(window=3, function=np.median, periods=2)
+    transformer.fit(df_time)
+    df_tr = transformer.transform(df_time)
+    print(df_tr.head())
+    # assert df_tr.columns.tolist() == expected_results_df.columns.tolist()
+    assert df_tr.head(9).equals(expected_results_df)
