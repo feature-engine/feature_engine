8c38267afaaff7264874b1417f0cfd67a2c9636e
Soledad Galli <solegalli@protonmail.com>
2022-03-14 10:33:59 +0100

    reorganises common checks

--- tests/test_wrappers/test_check_estimator_wrappers.py
+++ tests/test_wrappers/test_check_estimator_wrappers.py
@@ -7,8 +7,7 @@ from feature_engine.estimator_checks import (
     check_all_types_variables_assignment,
     check_feature_names_in,
     check_numerical_variables_assignment,
-    check_raises_error_when_fitting_not_a_df,
-    check_raises_error_when_transforming_not_a_df,
+    check_raises_error_when_input_not_a_df,
     check_raises_non_fitted_error,
 )
 from feature_engine.wrappers import SklearnTransformerWrapper
@@ -23,8 +22,7 @@ def test_sklearn_transformer_wrapper():
 )
 def test_check_estimator_from_feature_engine(estimator):
     check_raises_non_fitted_error(estimator)
-    check_raises_error_when_fitting_not_a_df(estimator)
-    check_raises_error_when_transforming_not_a_df(estimator)
+    check_raises_error_when_input_not_a_df(estimator)
     check_feature_names_in(estimator)
 
 
