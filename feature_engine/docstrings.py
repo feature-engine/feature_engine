b2df54b0712785f6bcea60722aee3bd49a9d5906
Soledad Galli <solegalli@protonmail.com>
2022-03-15 16:50:01 +0100

    fixes whitespace issue

--- feature_engine/docstrings.py
+++ feature_engine/docstrings.py
@@ -62,13 +62,13 @@ _fit_not_learn_docstring = """fit:
 
 _fit_transform_docstring = """fit_transform:
         Fit to data, then transform it.
-        
+
     get_feature_names_out:
         Get output feature names for transformation.
-        
+
     get_params:
         Get parameters for this estimator.
-        
+
     set_params:
         Set the parameters of this estimator.
         """.rstrip()
