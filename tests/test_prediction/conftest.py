7daf28e0d304ebc81e3239f4de2d982251c770ca
Soledad Galli <solegalli@protonmail.com>
2022-03-16 15:01:08 +0100

    adds tests for classifier

--- tests/test_prediction/conftest.py
+++ tests/test_prediction/conftest.py
@@ -3,73 +3,28 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def df_pred():
-    data = {
-        "Name": [
-            "tom",
-            "nick",
-            "krish",
-            "megan",
-            "peter",
-            "jordan",
-            "fred",
-            "sam",
-            "alexa",
-            "brittany"
-        ],
-        "City": [
-            "London",
-            "Manchester",
-            "Liverpool",
-            "Bristol",
-            "Manchester",
-            "Liverpool",
-            "London",
-            "Liverpool",
-            "Manchester",
-            "London"],
-        "Studies": [
-            "Bachelor",
-            "Bachelor",
-            "PhD",
-            "Masters",
-            "Bachelor",
-            "PhD",
-            "None",
-            "Masters",
-            "Masters",
-            "Bachelor"
-        ],
-        "Age": [20, 44, 19, 33, 51, 40, 41, 37, 30, 54],
-        "Height_cm": [164, 150, 178, 158, 188, 190, 168, 174, 176, 171],
-        "Marks": [1.0, 0.8, 0.6, 0.1, 0.3, 0.4, 0.8, 0.6, 0.5, 0.2],
-        "Plays_Football": [1, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+def df_classification():
+    df = {
+        "cat_var_A": ["A"] * 5 + ["B"] * 5 + ["C"] * 5 + ["D"] * 5,
+        "cat_var_B": ["A"] * 6 + ["B"] * 2 + ["C"] * 2 + ["B"] * 2 + ["C"] * 2 + ["D"] * 6,
+        "num_var_A": [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+        "num_var_B": [1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4],
     }
 
-    df = pd.DataFrame(data)
-
-    return df
+    df = pd.DataFrame(df)
+    y = pd.Series([0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1])
+    return df, y
 
 
 @pytest.fixture(scope="module")
-def df_pred_small():
-    data = {
-        "Names": ["louis", "michael", "arnold", "joyce", "claire", "allison"],
-        "City": [
-            "London",
-            "London",
-            "Liverpool",
-            "Manchester",
-            "Bristol",
-            "Manchester"
-        ],
-        "Studies": ["Masters", "Bachelor", "Bachelor", "PhD", "PhD", "Masters"],
-        "Age": [29, 39, 49, 25, 35, 55],
-        "Height_cm": [155, 170, 168, 184, 190, 174],
-        "Marks": [0.9, 0.6, 0.1, 0.5, 0.3, 0.8],
-        "Plays_Football": [0, 1, 1, 0, 1, 1],
+def df_regression():
+    df = {
+        "cat_var_A": ["A"] * 5 + ["B"] * 5 + ["C"] * 5 + ["D"] * 5,
+        "cat_var_B": ["A"] * 6 + ["B"] * 2 + ["C"] * 2 + ["B"] * 2 + ["C"] * 2 + ["D"] * 6,
+        "num_var_A": [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+        "num_var_B": [1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4],
     }
 
-    df = pd.DataFrame(data)
-
-    return df
+    df = pd.DataFrame(df)
+    y = pd.Series([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3])
+    return df, y
--- tests/test_prediction/test_target_mean_classifier.py
+++ tests/test_prediction/test_target_mean_classifier.py
@@ -1,262 +1,262 @@
 import numpy as np
 import pandas as pd
 import pytest
-from sklearn.exceptions import NotFittedError
+from feature_engine._prediction.target_mean_classifier import TargetMeanClassifier
 
-from feature_engine.discretisation import (
-    EqualFrequencyDiscretiser,
-)
-from feature_engine.encoding import MeanEncoder
-from feature_engine.prediction import TargetMeanClassifier
-
-_false_input_params = [
-    ("salsa", "arbitrary"),
-    ("33", "mean-encoder"),
-    ([7], True),
-    (False, "prost"),
-]
-
-_not_a_df = [
-    "not_a_df",
-    [0, -1, -2, "tree"],
-    pd.Series(["pen", "paper", 1984, "desk"], name="office_items"),
-]
+#TODO assert attribute classes
+#assert tr.classes_.tolist() == [0, 1]
 
 
-@pytest.mark.parametrize("_bins, _strategy", _false_input_params)
-def test_raises_error_when_wrong_input_params(_bins, _strategy):
-    with pytest.raises(ValueError):
-        assert TargetMeanClassifier(bins=_bins)
-    with pytest.raises(ValueError):
-        assert TargetMeanClassifier(strategy=_strategy)
+def test_classifier_categorical_variables(df_classification):
+
+    X, y = df_classification
+
+    tr = TargetMeanClassifier(variables="cat_var_A")
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
+
+    tr = TargetMeanClassifier(variables="cat_var_B")
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[1. , 0. ],
+       [1. , 0. ],
+       [1. , 0. ],
+       [1. , 0. ],
+       [1. , 0. ],
+       [1. , 0. ],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0. , 1. ],
+       [0. , 1. ],
+       [0. , 1. ],
+       [0. , 1. ],
+       [0. , 1. ],
+       [0. , 1. ]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
+
+    tr = TargetMeanClassifier(variables=["cat_var_A","cat_var_B"])
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[1.  , 0.  ],
+       [1.  , 0.  ],
+       [1.  , 0.  ],
+       [1.  , 0.  ],
+       [1.  , 0.  ],
+       [1.  , 0.  ],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.  , 1.  ],
+       [0.  , 1.  ],
+       [0.  , 1.  ],
+       [0.  , 1.  ],
+       [0.  , 1.  ],
+       [0.  , 1.  ]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
 
 
-def test_default_params():
-    transformer = TargetMeanClassifier()
-    assert isinstance(transformer, TargetMeanClassifier)
-    assert transformer.variables is None
-    assert transformer.bins == 5
-    assert transformer.strategy == "equal_width"
+def test_classifier_numerical_variables(df_classification):
+
+    X, y = df_classification
+
+    tr = TargetMeanClassifier(variables="num_var_A", bins=2)
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [1., 0.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.],
+                    [0., 1.]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
+
+    tr = TargetMeanClassifier(variables="cat_var_B")
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    exp_prob = np.array([[0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5],
+       [0.5, 0.5]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
+
+    tr = TargetMeanClassifier(variables=["num_var_A","num_var_B"])
+    tr.fit(X, y)
+
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.75, 0.25],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75],
+       [0.25, 0.75]])
+
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
 
 
-def test_attributes_upon_fitting(df_pred):
-    transformer = TargetMeanClassifier(
-        variables=None,
-        bins=7,
-        strategy="equal_frequency"
-    )
+def test_classifier_all_variables(df_classification):
 
-    transformer.fit(df_pred[["City", "Age"]], df_pred["Plays_Football"])
+    X, y = df_classification
 
-    # test init params
-    assert transformer.variables is None
-    assert transformer.bins == 7
-    assert transformer.strategy == "equal_frequency"
-    # test attributes
-    assert transformer.variables_categorical_ == ["City"]
-    assert transformer.variables_numerical_ == ["Age"]
-    assert transformer.classes_.tolist() == [0, 1]
-    assert type(transformer.pipeline_
-                .named_steps["discretiser"]) == EqualFrequencyDiscretiser
-    assert type(transformer.pipeline_
-                .named_steps["encoder_num"]) == MeanEncoder
-    assert type(transformer.pipeline_
-                .named_steps["encoder_cat"]) == MeanEncoder
-    assert transformer.n_features_in_ == 2
+    tr = TargetMeanClassifier(bins=2)
+    tr.fit(X, y)
 
+    pred = tr.predict(X)
+    prob = tr.predict_proba(X)
+    prob_log = tr.predict_proba(X)
 
-def test_classifier_results_with_two_numerical_variables(
-        df_pred, df_pred_small
-):
-    transformer = TargetMeanClassifier(
-        variables=None,
-        bins=6,
-        strategy="equal_width"
-    )
+    exp_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    exp_prob = np.array([[0.875, 0.125],
+       [0.875, 0.125],
+       [0.875, 0.125],
+       [0.875, 0.125],
+       [0.875, 0.125],
+       [0.875, 0.125],
+       [0.75 , 0.25 ],
+       [0.75 , 0.25 ],
+       [0.75 , 0.25 ],
+       [0.75 , 0.25 ],
+       [0.25 , 0.75 ],
+       [0.25 , 0.75 ],
+       [0.25 , 0.75 ],
+       [0.25 , 0.75 ],
+       [0.125, 0.875],
+       [0.125, 0.875],
+       [0.125, 0.875],
+       [0.125, 0.875],
+       [0.125, 0.875],
+       [0.125, 0.875]])
 
-    transformer.fit(df_pred[["Age", "Height_cm"]], df_pred["Plays_Football"])
-    pred = transformer.predict(df_pred_small[["Age", "Height_cm"]])
-    prob = transformer.predict_proba(df_pred_small[["Age", "Height_cm"]])
-    log_prob = transformer.predict_log_proba(df_pred_small[["Age", "Height_cm"]])
-    accuracy_score = transformer.score(
-        df_pred_small[["Age", "Height_cm"]], df_pred_small["Plays_Football"]
-    )
+    exp_prob_log = np.array([[-0.13353139, -2.07944154],
+       [-0.13353139, -2.07944154],
+       [-0.13353139, -2.07944154],
+       [-0.13353139, -2.07944154],
+       [-0.13353139, -2.07944154],
+       [-0.13353139, -2.07944154],
+       [-0.28768207, -1.38629436],
+       [-0.28768207, -1.38629436],
+       [-0.28768207, -1.38629436],
+       [-0.28768207, -1.38629436],
+       [-1.38629436, -0.28768207],
+       [-1.38629436, -0.28768207],
+       [-1.38629436, -0.28768207],
+       [-1.38629436, -0.28768207],
+       [-2.07944154, -0.13353139],
+       [-2.07944154, -0.13353139],
+       [-2.07944154, -0.13353139],
+       [-2.07944154, -0.13353139],
+       [-2.07944154, -0.13353139],
+       [-2.07944154, -0.13353139]])
 
-    # test predict results
-    assert pred.tolist() == [0, 1, 0, 0, 0, 0]
-    # test predict_proba results
-    assert prob.round(6).tolist() == [
-        [0.5, 0.5],
-        [0.416667, 0.583333],
-        [0.75, 0.25],
-        [0.75, 0.25],
-        [0.75, 0.25],
-        [0.833333, 0.166667]
-    ]
-    # test predict_log_proba results
-    assert log_prob.round(6).tolist() == [
-        [-0.693147, -0.693147],
-        [-0.875469, -0.538997],
-        [-0.287682, -1.386294],
-        [-0.287682, -1.386294],
-        [-0.287682, -1.386294],
-        [-0.182322, -1.791759]
-    ]
-    # test accuracy score calc
-    assert accuracy_score.round(6) == 0.5
-
-
-def test_classifier_results_with_two_categorical_variables(
-        df_pred, df_pred_small
-):
-    transformer = TargetMeanClassifier(
-        variables=None,
-        bins=4,
-        strategy="equal_frequency"
-    )
-
-    transformer.fit(df_pred[["Studies", "City"]], df_pred["Plays_Football"])
-    pred = transformer.predict(df_pred_small[["Studies", "City"]])
-    prob = transformer.predict_proba(df_pred_small[["Studies", "City"]])
-    log_prob = transformer.predict_log_proba(df_pred_small[["Studies", "City"]])
-
-    accuracy_score = transformer.score(
-        df_pred_small[["Studies", "City"]], df_pred_small["Plays_Football"]
-    )
-    # test predict results
-    assert pred.tolist() == [0, 0, 1, 1, 0, 0]
-    # test predict_proba results
-    assert prob.round(6).tolist() == [
-        [0.666667, 0.333333],
-        [0.583333, 0.416667],
-        [0.25, 0.75],
-        [0.333333, 0.666667],
-        [0.5, 0.5],
-        [0.666667, 0.333333]
-    ]
-    # test predict_log_proba results
-    assert log_prob.round(6).tolist() == [
-        [-0.405465, -1.098612],
-        [-0.538997, -0.875469],
-        [-1.386294, -0.287682],
-        [-1.098612, -0.405465],
-        [-0.693147, -0.693147],
-        [-0.405465, -1.098612]
-    ]
-    # test accuracy score calc
-    assert accuracy_score.round(6) == 0.333333
-
-
-def test_classifier_results_with_one_variable(
-        df_pred, df_pred_small
-):
-    transformer = TargetMeanClassifier(
-        variables=None,
-        bins=4,
-        strategy="equal_frequency"
-    )
-
-    transformer.fit(df_pred[["Studies"]], df_pred["Plays_Football"])
-    pred = transformer.predict(df_pred_small[["Studies"]])
-    prob = transformer.predict_proba(df_pred_small[["Studies"]])
-    log_prob = transformer.predict_log_proba(df_pred_small[["Studies"]])
-
-    accuracy_score = transformer.score(
-        df_pred_small[["Studies"]], df_pred_small["Plays_Football"]
-    )
-    # test predict results
-    assert pred.tolist() == [0, 0, 0, 1, 1, 0]
-    # test predict_proba results
-    assert prob.round(6).tolist() == [
-        [0.666667, 0.333333],
-        [0.5, 0.5],
-        [0.5, 0.5],
-        [0.0, 1.0],
-        [0.0, 1.0],
-        [0.666667, 0.333333]
-    ]
-    # test predict_log_proba results
-    assert log_prob.round(6).tolist() == [
-        [-0.405465, -1.098612],
-        [-0.693147, -0.693147],
-        [-0.693147, -0.693147],
-        [-float("inf"), 0.0],
-        [-float("inf"), 0.0],
-        [-0.405465, -1.098612]
-    ]
-    # test accuracy score calc
-    assert accuracy_score.round(6) == 0.333333
-
-
-def test_classifier_results_with_three_or_more_variables(
-        df_pred, df_pred_small
-):
-    transformer = TargetMeanClassifier(
-        variables=None,
-        bins=4,
-        strategy="equal_frequency"
-    )
-
-    transformer.fit(
-        df_pred[["Studies", "City", "Age", "Height_cm"]], df_pred["Plays_Football"]
-    )
-    pred = transformer.predict(df_pred_small[["Studies", "City", "Age", "Height_cm"]])
-    prob = transformer.predict_proba(df_pred_small[["Studies", "City", "Age", "Height_cm"]])
-    log_prob = transformer.predict_log_proba(
-        df_pred_small[["Studies", "City", "Age", "Height_cm"]]
-    )
-    accuracy_score = transformer.score(
-        df_pred_small[["Studies", "City", "Age", "Height_cm"]], df_pred_small["Plays_Football"]
-    )
-    # test predict results
-    assert pred.tolist() == [0, 0, 0, 1, 1, 0]
-    # test predict_proba results
-    assert prob.round(6).tolist() == [
-        [0.5, 0.5],
-        [0.666667, 0.333333],
-        [0.541667, 0.458333],
-        [0.333333, 0.666667],
-        [0.458333, 0.541667],
-        [0.625, 0.375]
-    ]
-    # test predict_log_proba results
-    assert log_prob.round(6).tolist() == [
-        [-0.693147, -0.693147],
-        [-0.405465, -1.098612],
-        [-0.613104, -0.780159],
-        [-1.098612, -0.405465],
-        [-0.780159, -0.613104],
-        [-0.470004, -0.980829]
-    ]
-    # test accuracy score calc
-    assert accuracy_score.round(6) == 0.333333
-
-
-def test_non_fitted_error(df_pred):
-    # test if transformer has been fitted
-    with pytest.raises(NotFittedError):
-        TargetMeanClassifier().predict(df_pred[["Studies", "Age"]])
-
-
-def test_raises_error_when_df_has_nan(df_enc_na):
-    # Raise error when dataset contains na, fit method
-    with pytest.raises(ValueError):
-        TargetMeanClassifier().fit(
-            df_enc_na[["var_A", "var_B"]], df_enc_na["target"]
-        )
-
-
-def test_error_if_df_contains_na_in_transform(df_enc, df_enc_na):
-    # raise error when dataset contains na, transform method
-    transformer = TargetMeanClassifier()
-    transformer.fit(df_enc[["var_A", "var_B"]], df_enc["target"])
-
-    with pytest.raises(ValueError):
-        transformer.predict(df_enc_na[["var_A", "var_B"]])
-
-
-@pytest.mark.parametrize("_not_a_df", _not_a_df)
-def test_raises_error_when_not_fitting_a_df(_not_a_df, df_pred):
-    transformer = TargetMeanClassifier()
-    with pytest.raises(TypeError):
-        transformer.fit(_not_a_df, df_pred["Plays_Football"])
+    np.array_equal(pred, exp_pred)
+    np.array_equal(prob, exp_prob)
+    np.array_equal(prob_log, exp_prob_log)
