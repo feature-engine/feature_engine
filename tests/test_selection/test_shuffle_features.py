f2b2b845af574e6c92524220a6ceecc361fcf77f
gverbock <32060943+gverbock@users.noreply.github.com>
2022-03-16 06:19:18 +0100

    Fixes CV split bug in SelectByShuffling  (#384)
    * Draft fix computing performance
    
    * lay-out
    
    * Remove assignment of y to pandas series
    
    * Enforce y has iloc attribute in shuffle feature selection
    
    * Remove unused argument in the test
    
    Co-authored-by: Gilles Verbockhaven <gilles.verbockhaven@ing.com>

--- tests/test_selection/test_shuffle_features.py
+++ tests/test_selection/test_shuffle_features.py
@@ -44,8 +44,9 @@ def test_sel_with_default_parameters(df_test):
 def test_regression_cv_3_and_r2(load_diabetes_dataset):
     #  test for regression using cv=3, and the r2 as metric.
     X, y = load_diabetes_dataset
+
     sel = SelectByShuffling(
-        estimator=LinearRegression(), scoring="r2", cv=3, threshold=0.01, random_state=1
+        estimator=LinearRegression(), scoring="r2", cv=3, threshold=0.05, random_state=1
     )
     sel.fit(X, y)
 
@@ -55,7 +56,7 @@ def test_regression_cv_3_and_r2(load_diabetes_dataset):
     # test init params
     assert sel.cv == 3
     assert sel.scoring == "r2"
-    assert sel.threshold == 0.01
+    assert sel.threshold == 0.05
     # fit params
     assert np.round(sel.initial_model_performance_, 3) == 0.489
     assert sel.features_to_drop_ == [0, 6, 7, 9]
@@ -72,22 +73,22 @@ def test_regression_cv_2_and_mse(load_diabetes_dataset):
         estimator=DecisionTreeRegressor(random_state=0),
         scoring="neg_mean_squared_error",
         cv=2,
-        threshold=5,
+        threshold=1000,
         random_state=1,
     )
     # fit transformer
     sel.fit(X, y)
 
     # expected output
-    Xtransformed = X[[2, 8]].copy()
+    Xtransformed = X[[2, 7, 8]].copy()
 
     # test init params
     assert sel.cv == 2
     assert sel.scoring == "neg_mean_squared_error"
-    assert sel.threshold == 5
+    assert sel.threshold == 1000
     # fit params
     assert np.round(sel.initial_model_performance_, 0) == -5836.0
-    assert sel.features_to_drop_ == [0, 1, 3, 4, 5, 6, 7, 9]
+    assert sel.features_to_drop_ == [0, 1, 3, 4, 5, 6, 9]
     # test transform output
     pd.testing.assert_frame_equal(sel.transform(X), Xtransformed)
 
