6a21754d1241580a199bf023777a3f0252f21434
Soledad Galli <solegalli@protonmail.com>
2022-03-26 08:02:00 +0100

    changes wording in relative features

--- feature_engine/creation/relative_features.py
+++ feature_engine/creation/relative_features.py
@@ -50,8 +50,9 @@ class RelativeFeatures(BaseCreation):
     functions are added to the dataframe.
 
     This transformer works only with numerical variables. It uses the pandas methods
-    pd.DataFrme.add, pd.DataFrme.sub, pd.DataFrme.mul, pd.DataFrme.div,
-    pd.DataFrme.truediv, pd.DataFrme.floordiv, pd.DataFrme.mod and pd.DataFrme.pow.
+    `pd.DataFrme.add`, `pd.DataFrme.sub`, `pd.DataFrme.mul`, `pd.DataFrme.div`,
+    `pd.DataFrme.truediv`, `pd.DataFrme.floordiv`, `pd.DataFrme.mod` and
+    `pd.DataFrme.pow`.
     Find out more in `pandas documentation
     <https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.add.html>`_.
 
