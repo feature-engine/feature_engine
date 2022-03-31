f90c63f93ee1a254843ec8087555264be859878c
Noah Green <noahjgreen295@gmail.com>
2022-03-30 22:45:10 -0400

    added numpy_to_pandas functionality, subroutines to be used in several other places. Includes unit tests.

---
+++ feature_engine/numpy_to_pandas.py
@@ -0,0 +1,62 @@
+"""Functions to detect numpy objects and convert to pandas objects."""
+
+from typing import Any, Union, List
+import numpy as np
+import pandas as pd
+
+def _is_numpy(obj_in: Any) -> bool:
+    """
+    Tests if an object is a numpy object.
+    If the input is a numpy array, it converts it to a pandas Dataframe. This is mostly
+    so that we can add the check_estimator checks for compatibility with sklearn.
+
+    Parameters
+    ----------
+    obj_in : the object to test.
+
+    Returns
+    -------
+    True if object is a numpy object, else False
+    """
+    return isinstance(obj_in, (np.generic, np.ndarray))
+
+
+def _numpy_to_dataframe(obj_in: Union[np.generic, np.ndarray], index=None) -> pd.DataFrame:
+    """
+    Converts a numpy object to a pandas DataFrame
+
+    Parameters
+    ----------
+    obj_in : the object to convert
+    index : array-like (optional); will set index on DataFrame
+
+    Returns
+    -------
+    df_out : the object converted to a pandas DataFrame
+    """
+    col_names: List[str] = [str(i) for i in range(obj_in.shape[1])]
+    df_out: pd.DataFrame = pd.DataFrame(obj_in, columns=col_names)
+    if index is not None:
+        df_out.index = index
+
+    return df_out
+
+
+def _numpy_to_series(obj_in: Union[np.generic, np.ndarray], index=None) -> pd.Series:
+    """
+    Converts a numpy object to a pandas Series
+
+    Parameters
+    ----------
+    obj_in : the object to convert
+    index : array-like (optional); will set index on Series
+
+    Returns
+    -------
+    df_out : the object converted to a pandas Series
+    """
+    s_out: pd.Series = pd.Series(obj_in)
+    if index is not None:
+        s_out.index = index
+
+    return s_out
---
+++ tests/test_numpy_to_pandas.py
@@ -0,0 +1,42 @@
+from typing import Any
+import numpy as np
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal, assert_series_equal
+
+
+from feature_engine.numpy_to_pandas import (
+    _is_numpy,
+    _numpy_to_series,
+    _numpy_to_dataframe
+)
+
+
+@pytest.mark.parametrize(
+    "obj, expected",
+    [
+        (np.array([1, 2, 3, 4]), True),
+        (pd.Series([1, 2, 3, 4]), False),
+        ("something", False)
+    ]
+)
+def test_is_numpy(obj: Any, expected: bool):
+    assert _is_numpy(obj) == expected
+
+
+def test_numpy_to_dataframe():
+    np_array: np.ndarray = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    expected: pd.DataFrame = pd.DataFrame({"0": [1, 4, 7], "1": [2, 5, 8], "2": [3, 6, 9]})
+    assert_frame_equal(_numpy_to_dataframe(np_array), expected)
+
+    expected.index = ["a", "b", "c"]
+    assert_frame_equal(_numpy_to_dataframe(np_array, index=["a", "b", "c"]), expected)
+
+
+def test_numpy_to_series():
+    np_array: np.ndarray = np.array([1, 2, 3])
+    expected: pd.Series = pd.Series([1, 2, 3])
+    assert_series_equal(_numpy_to_series(np_array), expected)
+
+    expected.index = ["a", "b", "c"]
+    assert_series_equal(_numpy_to_series(np_array, index=["a", "b", "c"]), expected)
