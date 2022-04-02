d5ea00c3ba9b4b90e077934cb0aee560c80ef9c2
Noah Green <noahjgreen295@gmail.com>
2022-04-02 19:27:33 -0400

    additional flake8

--- tests/test_numpy_to_pandas.py
+++ tests/test_numpy_to_pandas.py
@@ -26,7 +26,9 @@ def test_is_numpy(obj: Any, expected: bool):
 
 def test_numpy_to_dataframe():
     np_array: np.ndarray = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    expected: pd.DataFrame = pd.DataFrame({"0": [1, 4, 7], "1": [2, 5, 8], "2": [3, 6, 9]})
+    expected: pd.DataFrame = pd.DataFrame(
+        {"0": [1, 4, 7], "1": [2, 5, 8], "2": [3, 6, 9]}
+    )
     assert_frame_equal(_numpy_to_dataframe(np_array), expected)
 
     expected.index = ["a", "b", "c"]
@@ -40,3 +42,4 @@ def test_numpy_to_series():
 
     expected.index = ["a", "b", "c"]
     assert_series_equal(_numpy_to_series(np_array, index=["a", "b", "c"]), expected)
+
