5c967d18d4f2d6faa46b2361fd72c4eace2c113d
Soledad Galli <solegalli@protonmail.com>
2022-04-06 19:16:29 +0200

    undo isort

--- feature_engine/creation/__init__.py
+++ feature_engine/creation/__init__.py
@@ -2,13 +2,13 @@
 The module creation includes classes to create new variables by combination of existing
 variables in the dataframe.
 """
+from .cyclical_features import CyclicalFeatures
+from .math_features import MathFeatures
+from .relative_features import RelativeFeatures
 # FIXME: remove in version 1.4
 from .combine_with_reference_feature import CombineWithReferenceFeature
 from .cyclical import CyclicalTransformer
-from .cyclical_features import CyclicalFeatures
-from .math_features import MathFeatures
 from .mathematical_combination import MathematicalCombination
-from .relative_features import RelativeFeatures
 
 __all__ = [
     "MathematicalCombination",
