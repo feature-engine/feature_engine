cca7b4d814a1f9e285bde91dd760294c2f48b81c
Noah Green <noahjgreen295@gmail.com>
2022-04-06 11:46:52 -0400

    Fix DropFeatures fail when features_to_drop is string, fixes issue 406 (#407)
    * added unit test reproducing bug; test now fails
    
    * fixed type checking
    
    * store single string attribute as single-element list for consistency; unit test now passing
    
    * black/isort/flake8
    
    * undid reassignment of string to list
    
    * small addition to exception message

--- feature_engine/selection/drop_features.py
+++ feature_engine/selection/drop_features.py
@@ -52,10 +52,10 @@ class DropFeatures(BaseSelector):
 
     def __init__(self, features_to_drop: List[Union[str, int]]):
 
-        if not isinstance(features_to_drop, list) or len(features_to_drop) == 0:
+        if not isinstance(features_to_drop, (str, list)) or len(features_to_drop) == 0:
             raise ValueError(
-                "features_to_drop should be a list with the name of the variables"
-                "you wish to drop from the dataframe."
+                f"features_to_drop should be a list with the name of the variables "
+                f"you wish to drop from the dataframe. Got {features_to_drop} instead."
             )
 
         self.features_to_drop = features_to_drop
