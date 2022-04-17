c738ead1d43e60ab57c078f23ef611565aa31e5f
Morgan-Sell <morganpsell@gmail.com>
2022-04-17 12:52:12 -0700

    update wrappers.py

--- feature_engine/wrappers/wrappers.py
+++ feature_engine/wrappers/wrappers.py
@@ -6,7 +6,9 @@ from sklearn.utils.validation import check_is_fitted
 
 from feature_engine.dataframe_checks import _check_X_matches_training_df, check_X
 from feature_engine.parameter_checks import check_input_features
-from feature_engine._docstrings.methods import _input_features_docstring
+from feature_engine._docstrings.methods import (
+    _get_feature_names_out_docstring
+)
 from feature_engine._docstrings.substitute import Substitution
 from feature_engine.tags import _return_tags
 from feature_engine.variable_manipulation import (
@@ -347,21 +349,12 @@ class SklearnTransformerWrapper(BaseEstimator, TransformerMixin):
         return X
 
     @Substitution(
-        input_features=_input_features_docstring,
+        get_feature_names_out=_get_feature_names_out_docstring,
     )
     def get_feature_names_out(
             self, input_features: Optional[Union[List, str]] = None
     ) -> List:
-        """Get output feature names for transformation.
-
-        Parameters
-        ----------
-        {input_features}
-
-        Returns
-        -------
-        feature_names_out: list
-            The feature names.
+        """{get_feature_names_out}
         """
         # Check method fit has been called
         check_is_fitted(self)
