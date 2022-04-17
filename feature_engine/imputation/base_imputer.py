cd59ff928e2894592c6720dc055fdf40d91587eb
Morgan-Sell <morganpsell@gmail.com>
2022-04-16 21:07:08 -0700

    update base_encoder, base_imputer, base_outlier, and match_columns

--- feature_engine/imputation/base_imputer.py
+++ feature_engine/imputation/base_imputer.py
@@ -6,7 +6,7 @@ from sklearn.utils.validation import check_is_fitted
 
 from feature_engine.dataframe_checks import _check_X_matches_training_df, check_X
 from feature_engine.parameter_checks import check_input_features
-from feature_engine._docstrings.methods import _input_features_docstring
+from feature_engine._docstrings.methods import _get_feature_names_out_docstring
 from feature_engine._docstrings.substitute import Substitution
 from feature_engine.tags import _return_tags
 
@@ -91,21 +91,12 @@ class BaseImputer(BaseEstimator, TransformerMixin):
         return self
 
     @Substitution(
-        input_features=_input_features_docstring,
+        get_feature_names_out=_get_feature_names_out_docstring,
     )
     def get_feature_names_out(
             self, input_features: Optional[Union[List, str]] = None
     ) -> List:
-        """Get output feature names for transformation.
-
-        Parameters
-        ----------
-        {input_features}
-
-        Returns
-        -------
-        feature_names_out: list
-            The feature names.
+        """{get_feature_names_out}
         """
         check_is_fitted(self)
         feature_names = check_input_features(input_features, self.variables_)
