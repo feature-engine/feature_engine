e5f15a9e6202fdc45564341128e7c3532728d1c2
Morgan-Sell <morganpsell@gmail.com>
2022-04-16 20:50:58 -0700

    change _input_features_docting to _get_feature_names_out_docstring

--- feature_engine/_docstrings/methods.py
+++ feature_engine/_docstrings/methods.py
@@ -19,8 +19,20 @@ _inverse_transform_docstring = """inverse_transform:
         Convert the data back to the original representation.
         """.rstrip()
 
-_input_features_docstring = """input_features: str, list, default=None
-        Input features. If `None`, then the names of all the variables in the
-        transformed dataset (original + new variables) is returned. If list
-        with feature names, the features in the list will be returned."
-    """.rstrip()
+_get_feature_names_out_docstring = """
+        Get output feature names for transformation.
+        
+        Parameters
+        ----------
+        input_features: str, list, default=None
+            If `None`, then the names of all the variables in the 
+            transformed dataset is returned. If list with feature 
+            names, the features in the list will be returned. This 
+            parameter exists mostly for compatibility with the 
+            Scikit-learn Pipeline.
+        
+        Returns
+        -------
+        feature_names_out: list
+            The feature names.
+        """.rstrip()
