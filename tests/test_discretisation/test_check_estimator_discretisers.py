0ac284c07b415f97a4bad941b495aca006aac157
Morgan-Sell <morganpsell@gmail.com>
2022-04-19 16:33:06 -0700

    add TargetMeanDiscretiser to test_check_estimator_discretisers.py

--- tests/test_discretisation/test_check_estimator_discretisers.py
+++ tests/test_discretisation/test_check_estimator_discretisers.py
@@ -7,6 +7,7 @@ from feature_engine.discretisation import (
     DecisionTreeDiscretiser,
     EqualFrequencyDiscretiser,
     EqualWidthDiscretiser,
+    TargetMeanDiscretiser,
 )
 from tests.estimator_checks.estimator_checks import check_feature_engine_estimator
 
@@ -14,7 +15,8 @@ _estimators = [
     DecisionTreeDiscretiser(regression=False),
     EqualFrequencyDiscretiser(),
     EqualWidthDiscretiser(),
-    ArbitraryDiscretiser(binning_dict={"0": [-np.Inf, 0, np.Inf]}),
+    ArbitraryDiscretiser(binning_dict={"0": [-np.Inf, 0, np.Inf]})
+    TargetMeanDiscretiser(),
 ]
 
 
