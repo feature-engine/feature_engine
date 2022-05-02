d04e4183ad08aa79a0b007e71c498de61c98e99f
Soledad Galli <solegalli@protonmail.com>
2022-05-02 11:36:09 +0200

    modify method in base outlier

--- feature_engine/outliers/base_outlier.py
+++ feature_engine/outliers/base_outlier.py
@@ -11,10 +11,8 @@ from feature_engine.dataframe_checks import (
     _check_X_matches_training_df,
     check_X,
 )
-from feature_engine.parameter_checks import check_input_features
-from feature_engine._docstrings.methods import (
-    _get_feature_names_out_docstring,
-)
+from feature_engine.get_feature_names_out import _get_feature_names_out
+from feature_engine._docstrings.methods import _get_feature_names_out_docstring
 from feature_engine._docstrings.substitute import Substitution
 from feature_engine.tags import _return_tags
 from feature_engine.variable_manipulation import (
@@ -112,15 +110,21 @@ class BaseOutlier(BaseEstimator, TransformerMixin):
         return X
 
     @Substitution(
-        get_feature_names_out=_get_feature_names_out_docstring,
+        get_feature_names_out=_get_feature_names_out_docstring
     )
     def get_feature_names_out(
-            self, input_features: Optional[Union[List, str]] = None
-    ) -> Union[List, str]:
-        """{get_feature_names_out}
-        """
+            self, input_features: Optional[List[int, str]] = None
+    ) -> List[int, str]:
+        """{get_feature_names_out}"""
+
         check_is_fitted(self)
-        feature_names = check_input_features(input_features, self.variables_)
+
+        feature_names = _get_feature_names_out(
+            features_in=self.feature_names_in_,
+            transformed_features=self.variables_,
+            input_features=input_features,
+        )
+
         return feature_names
 
     def _more_tags(self):
