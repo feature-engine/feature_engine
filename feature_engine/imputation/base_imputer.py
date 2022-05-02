67ec37a0e2738e65e46cd2d549afe522696bc835
Soledad Galli <solegalli@protonmail.com>
2022-05-02 11:33:41 +0200

    modify method in base imputer

--- feature_engine/imputation/base_imputer.py
+++ feature_engine/imputation/base_imputer.py
@@ -5,7 +5,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 
 from feature_engine.dataframe_checks import _check_X_matches_training_df, check_X
-from feature_engine.parameter_checks import check_input_features
+from feature_engine.get_feature_names_out import _get_feature_names_out
 from feature_engine._docstrings.methods import _get_feature_names_out_docstring
 from feature_engine._docstrings.substitute import Substitution
 from feature_engine.tags import _return_tags
@@ -91,15 +91,21 @@ class BaseImputer(BaseEstimator, TransformerMixin):
         return self
 
     @Substitution(
-        get_feature_names_out=_get_feature_names_out_docstring,
+        get_feature_names_out=_get_feature_names_out_docstring
     )
     def get_feature_names_out(
-            self, input_features: Optional[Union[List, str]] = None
-    ) -> Union[List, str]:
-        """{get_feature_names_out}
-        """
+            self, input_features: Optional[List[int, str]] = None
+    ) -> List[int, str]:
+        """{get_feature_names_out}"""
+
         check_is_fitted(self)
-        feature_names = check_input_features(input_features, self.variables_)
+
+        feature_names = _get_feature_names_out(
+            features_in=self.feature_names_in_,
+            transformed_features=self.variables_,
+            input_features=input_features,
+        )
+
         return feature_names
 
     def _more_tags(self):
