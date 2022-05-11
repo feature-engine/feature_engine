106b1cd2b4a3ac649389631129faa9ad7ed00491
Tommaso Pellegrino <tommasopellegrino@Air-di-Tommaso.home-life.hub>
2022-05-11 09:39:47 +0200

    reintegrates useful tests, deletes test we can't run for the range limitation

--- tests/test_transformation/test_arcsin_transformer.py
+++ tests/test_transformation/test_arcsin_transformer.py
@@ -4,39 +4,19 @@ from sklearn.exceptions import NotFittedError
 
 from feature_engine.transformation import ArcsinTransformer
 
-
-#def test_automatically_find_variables(df_vartypes):
-#    # test case 1: automatically select variables
-#    transformer = ArcsinTransformer(variables=None)
-#
-#    # test fails here because of the df_vartypes data that are passed raise ValueError 
-#    X = transformer.fit_transform(df_vartypes)
-#
-#    # expected output
-#    transf_df = df_vartypes.copy()
-#    transf_df["Age"] = [0.05, 0.047619, 0.0526316, 0.0555556]
-#    transf_df["Marks"] = [1.0000, 0.200, 0.34637 , 0]
-#
-#    # test inverse_transform
-#    Xit = transformer.inverse_transform(X)
-#
-#    # convert numbers to original format.
-#    Xit["Age"] = Xit["Age"].round().astype("int64")
-#    Xit["Marks"] = Xit["Marks"].round(1)
-
-#def test_fit_raises_error_if_na_in_df(df_na):
-#    # test case 2: when dataset contains na, fit method
-#    with pytest.raises(ValueError):
-#        transformer = ArcsinTransformer()
-#        transformer.fit(df_na)
+def test_fit_raises_error_if_na_in_df(df_na):
+    # test case 2: when dataset contains na, fit method
+    with pytest.raises(ValueError):
+        transformer = ArcsinTransformer()
+        transformer.fit(df_na)
 
 
-#def test_transform_raises_error_if_na_in_df(df_vartypes, df_na):
-#    # test case 3: when dataset contains na, transform method
-#    with pytest.raises(ValueError):
-#        transformer = ArcsinTransformer()
-##        transformer.fit(df_vartypes)
-#        transformer.transform(df_na[["Name", "City", "Age", "Marks", "dob"]])
+def test_transform_raises_error_if_na_in_df(df_vartypes, df_na):
+    # test case 3: when dataset contains na, transform method
+    with pytest.raises(ValueError):
+        transformer = ArcsinTransformer()
+        transformer.fit(df_vartypes)
+        transformer.transform(df_na[["Name", "City", "Age", "Marks", "dob"]])
 
 
 def test_error_if_df_contains_outside_range_value(df_vartypes):
@@ -56,7 +36,7 @@ def test_error_if_df_contains_outside_range_value(df_vartypes):
         transformer.transform(df_neg)
 
 
-#def test_non_fitted_error(df_vartypes):
-#    with pytest.raises(NotFittedError):
-#        transformer = ArcsinTransformer()
-#        transformer.transform(df_vartypes)
+def test_non_fitted_error(df_vartypes):
+    with pytest.raises(NotFittedError):
+        transformer = ArcsinTransformer()
+        transformer.transform(df_vartypes)
