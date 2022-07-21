7973f252f67c17be1f107764a8937676d319045e
Morgan-Sell <morganpsell@gmail.com>
2022-07-21 15:45:59 -0700

    create test_information_value_encoder_dict()

--- tests/test_selection/test_information_value.py
+++ tests/test_selection/test_information_value.py
@@ -4,6 +4,33 @@ import pytest
 from feature_engine.selection import InformationValue
 
 
+def _round_dict_of_dict_values(data, ndigits):
+    """
+    Rounds the values of the encoder dictionaries to a given precision
+    in decimal digits.
+
+    Parameters:
+    -----------
+    data: dict
+        Dictionary of dictionary. Each variables encoder dictionary.
+
+    ndigits: int
+        Precision of decimal digits to round
+
+    Returns:
+    --------
+    dict_rounded: dict
+        Dictionary of dictionaries with rounded values.
+    """
+    dict_rounded = {var_name: {} for var_name in data.keys()}
+    variables = list(data.keys())
+    encoder_dict_values = list(data.values())
+    for var, temp_dict in zip(variables, encoder_dict_values):
+        for key, val in temp_dict.items():
+            dict_rounded[var][key] = round(val, ndigits)
+    return dict_rounded
+
+
 def test_class_diff_encoder_dict_calc(df_enc):
     transformer = InformationValue(
         variables=None,
@@ -15,12 +42,7 @@ def test_class_diff_encoder_dict_calc(df_enc):
     encoder_dict = transformer.class_diff_encoder_dict_
 
     # create dictionary with rounded values for assert statement
-    dict_rounded = {var_name:{} for var_name in encoder_dict.keys()}
-    variables = list(encoder_dict.keys())
-    encoder_dict_values = list(encoder_dict.values())
-    for var, temp_dict in zip(variables, encoder_dict_values):
-        for key, val in temp_dict.items():
-            dict_rounded[var][key] = round(val, 5)
+    dict_rounded = _round_dict_of_dict_values(encoder_dict, 5)
 
     expected_results = {
         "var_A": {"A": 0.04762, "B": -0.2381, "C": 0.19048},
@@ -30,3 +52,22 @@ def test_class_diff_encoder_dict_calc(df_enc):
     assert dict_rounded == expected_results
 
 
+def test_information_value_encoder_dict(df_enc):
+    transformer = InformationValue(
+        variables=None,
+        confirm_variables=False,
+        ignore_format=False,
+        errors="ignore"
+    )
+    transformer.fit(df_enc[["var_A", "var_B"]], df_enc["target"])
+    encoder_dict = transformer.info_value_encoder_dict_
+
+    # create dictionary with rounded values for assert statement
+    dict_rounded = _round_dict_of_dict_values(encoder_dict, 5)
+
+    expected_results = {
+        "var_A": {"A": 0.00734, "B": 0.12833, "C": 0.16139},
+        "var_B": {"A": 0.12833, "B": 0.00734, "C": 0.16139},
+    }
+
+    assert dict_rounded == expected_results
