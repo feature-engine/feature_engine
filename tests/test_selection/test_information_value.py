fa394dcd4b987600db7602e085d9a202714d6de3
Morgan-Sell <morganpsell@gmail.com>
2022-07-26 16:46:36 -0700

    create two test_error unit tests

--- tests/test_selection/test_information_value.py
+++ tests/test_selection/test_information_value.py
@@ -34,7 +34,7 @@ def _round_dict_of_dict_values(data, ndigits):
 def test_class_diff_encoder_dict_calc(df_enc):
     transformer = InformationValue(
         variables=None,
-        confirm_variables=False,
+        sort_values=False,
         ignore_format=False,
         errors="ignore"
     )
@@ -52,22 +52,47 @@ def test_class_diff_encoder_dict_calc(df_enc):
     assert dict_rounded == expected_results
 
 
-def test_information_value_encoder_dict(df_enc):
+def test_information_value_calc(df_enc):
     transformer = InformationValue(
         variables=None,
-        confirm_variables=False,
+        sort_values=False,
         ignore_format=False,
         errors="ignore"
     )
     transformer.fit(df_enc[["var_A", "var_B"]], df_enc["target"])
-    encoder_dict = transformer.info_value_encoder_dict_
+    information_values = transformer.information_values_
 
     # create dictionary with rounded values for assert statement
-    dict_rounded = _round_dict_of_dict_values(encoder_dict, 5)
-
-    expected_results = {
-        "var_A": {"A": 0.00734, "B": 0.12833, "C": 0.16139},
-        "var_B": {"A": 0.12833, "B": 0.00734, "C": 0.16139},
+    info_vals_rounded = {
+        var: round(val, 5) for var, val in information_values.items()
     }
 
-    assert dict_rounded == expected_results
+    expected_results = {"var_A": 0.29706, "var_B": 0.29706}
+
+    assert info_vals_rounded == expected_results
+
+
+def test_error_when_more_than_two_classes(df_enc_numeric):
+    transformer = InformationValue(
+        variables=None,
+        sort_values=True,
+        ignore_format=False,
+        errors="ignore"
+    )
+    with pytest.raises(ValueError):
+        transformer.fit(
+            df_enc_numeric[["var_A", "target"]], df_enc_numeric["var_B"]
+        )
+
+
+@pytest.mark.parametrize(
+    "_sort_values", [3, "python", (True, False), ["var", "fun"]]
+)
+def test_error_when_not_permitted_param_sort_values(_sort_values):
+    with pytest.raises(ValueError):
+        InformationValue(
+            variables=None,
+            sort_values=_sort_values,
+            ignore_format=True,
+            errors="ignore"
+        )
