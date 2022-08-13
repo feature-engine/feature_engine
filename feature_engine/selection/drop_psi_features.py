075667b9807367245798029287aa2200043ed532
Gleb Levitski <36483986+GLevV@users.noreply.github.com>
2022-08-13 07:23:46 +0000

    smallfix

--- feature_engine/selection/drop_psi_features.py
+++ feature_engine/selection/drop_psi_features.py
@@ -250,8 +250,10 @@ class DropHighPSIFeatures(BaseSelector):
         if not isinstance(switch, bool):
             raise ValueError(f"switch must be a boolean. Got {switch} instead.")
 
-        if not isinstance(threshold, (float, int)) or threshold < 0 or threshold != 'auto':
-            raise ValueError(f"threshold must be >= 0 or 'auto'. Got {threshold} instead.")
+        if isinstance(threshold, str) and threshold != 'auto':
+            raise ValueError(f"threshold must be float or 'auto'. Got {threshold} instead.")
+        elif not isinstance(threshold, (float, int)) or threshold < 0:
+            raise ValueError(f"threshold must be >= 0. Got {threshold} instead.")
 
         if not isinstance(bins, int) or bins <= 1:
             raise ValueError(f"bins must be an integer >= 1. Got {bins} instead.")
