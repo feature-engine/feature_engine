27302edf0af372fc173146b8ce75004cc2f53785
Soledad Galli <solegalli@protonmail.com>
2022-08-18 13:23:11 +0200

    refactors dictionary check

--- feature_engine/parameter_checks.py
+++ feature_engine/parameter_checks.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 
-def _define_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
+def _check_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
     """
     Checks if all values in dictionary are integers and floats. Can take None as
     argument.
@@ -17,20 +17,13 @@ def _define_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
         If any of the values in the dictionary are not int or float
     TypeError
         When argument type is not a dictionary.
-
-    Returns
-    -------
-    None or the input dictionary
     """
 
-    if not dict_:
-        dict_ = dict_
-
-    elif isinstance(dict_, dict):
+    if isinstance(dict_, dict):
         if not all([isinstance(x, (float, int)) for x in dict_.values()]):
             raise ValueError("All values in the dictionary must be integer or float")
 
-    else:
+    elif dict_ is not None:
         raise TypeError("The parameter can only take a dictionary or None")
 
-    return dict_
+    return None
