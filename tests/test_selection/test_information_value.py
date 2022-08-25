ee73b74fb9f28a75849f928b2220c46f70a38199
Morgan-Sell <morganpsell@gmail.com>
2022-08-25 15:58:19 -0700

    expand unit tests

--- tests/test_selection/test_information_value.py
+++ tests/test_selection/test_information_value.py
@@ -30,3 +30,55 @@ def test_error_when_more_than_two_classes(df_enc_numeric):
         )
 
 
+def test_error_when_no_categorical_values(df_test):
+    X, y = df_test
+
+    transformer = SelectByInformationValue(
+        variables=None,
+        threshold=0.2,
+        ignore_format=False,
+        confirm_variables=False
+    )
+    with pytest.raises(ValueError):
+        transformer.fit_transform(X, y)
+
+
+def test_transformer_with_default_params(df_enc):
+    X = df_enc.drop("target", axis=1)
+    y = df_enc["target"].copy()
+
+    transformer = SelectByInformationValue(
+        variables=None,
+        threshold=0.2,
+        ignore_format=False,
+        confirm_variables=False
+    )
+    X_tr = transformer.fit_transform(X, y)
+
+    expected_results = {
+        'var_E': {
+            0: 'R',
+            1: 'R',
+            2: 'R',
+            3: 'R',
+            4: 'R',
+            5: 'R',
+            6: 'R',
+            7: 'S',
+            8: 'S',
+            9: 'S',
+            10: 'S',
+            11: 'T',
+            12: 'T',
+            13: 'T',
+            14: 'T',
+            15: 'T',
+            16: 'T',
+            17: 'T',
+            18: 'T',
+            19: 'T'
+        }
+    }
+    expected_results_df = pd.DataFrame(expected_results)
+
+    assert X_tr.equals(expected_results_df)
