f6f7bf15e8b9b828664d9395f8c208a064c6484b
Soledad Galli <solegalli@protonmail.com>
2022-10-11 11:24:42 +0200

    rewords docstrings get features names

--- feature_engine/encoding/similarity_encoder.py
+++ feature_engine/encoding/similarity_encoder.py
@@ -261,7 +261,7 @@ class StringSimilarityEncoder(CategoricalInitMixin, CategoricalMethodsMixin):
         return feature_names
 
     def _add_new_feature_names(self, feature_names) -> List:
-        """Adds new features to df columns, and removes categoricals."""
+        """Creates new features names and removes original categorical variables."""
         feature_names = feature_names + self._get_new_features_name()
         feature_names = [f for f in feature_names if f not in self.variables_]
 
