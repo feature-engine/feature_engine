97a70ab6a5205756415c9474d2d56eddd3cb1258
Soledad Galli <solegalli@protonmail.com>
2022-12-08 14:50:44 +0100

    reformat and expand tests

--- feature_engine/encoding/_helper_functions.py
+++ feature_engine/encoding/_helper_functions.py
@@ -1,4 +1,11 @@
 def check_parameter_unseen(errors, accepted_values):
+    if not isinstance(accepted_values, list) or not all(
+        isinstance(item, str) for item in accepted_values
+    ):
+        raise ValueError(
+            "accepted_values should be a list of strings. "
+            f" Got {accepted_values} instead."
+        )
     if errors not in accepted_values:
         raise ValueError(
             f"errors takes only values {', '.join(accepted_values)}."
