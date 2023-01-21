01f8364393b313c1e721844e16c33ddeb16e07ec
Gleb Levitski <36483986+glevv@users.noreply.github.com>
2023-01-21 08:45:42 +0000

    Update test_increasing_width_discretiser.py

--- tests/test_discretisation/test_increasing_width_discretiser.py
+++ tests/test_discretisation/test_increasing_width_discretiser.py
@@ -2,11 +2,11 @@ import numpy as np
 import pytest
 from sklearn.exceptions import NotFittedError
 
-from feature_engine.discretisation import IncreasingWidthDiscretiser
+from feature_engine.discretisation import GeometricWidthDiscretiser
 
 
 def test_init_and_fit_params(df_normal_dist):
-    transformer = IncreasingWidthDiscretiser(
+    transformer = GeometricWidthDiscretiser(
         bins=10, variables=None, return_object=False
     )
     transformer.fit(df_normal_dist)
@@ -20,7 +20,7 @@ def test_init_and_fit_params(df_normal_dist):
 
 
 def test_fit_and_transform_methods(df_normal_dist):
-    transformer = IncreasingWidthDiscretiser(
+    transformer = GeometricWidthDiscretiser(
         bins=10, variables=None, return_object=False
     )
     X = transformer.fit_transform(df_normal_dist)
@@ -35,11 +35,11 @@ def test_fit_and_transform_methods(df_normal_dist):
     assert (transformer.binner_dict_["var"] == bins).all()
 
     # transform params
-    assert (X == np.digitize(df_normal_dist, bins)).all()
+    assert (X["var"] == np.digitize(df_normal_dist, bins)).all()
 
 
 def test_automatically_find_variables_and_return_as_object(df_normal_dist):
-    transformer = IncreasingWidthDiscretiser(
+    transformer = GeometricWidthDiscretiser(
         bins=10, variables=None, return_object=True
     )
     X = transformer.fit_transform(df_normal_dist)
@@ -48,30 +48,30 @@ def test_automatically_find_variables_and_return_as_object(df_normal_dist):
 
 def test_error_when_bins_not_number():
     with pytest.raises(ValueError):
-        IncreasingWidthDiscretiser(bins="other")
+        GeometricWidthDiscretiser(bins="other")
 
 
 def test_error_if_return_object_not_bool():
     with pytest.raises(ValueError):
-        IncreasingWidthDiscretiser(return_object="other")
+        GeometricWidthDiscretiser(return_object="other")
 
 
 def test_error_if_input_df_contains_na_in_fit(df_na):
     # test case 3: when dataset contains na, fit method
-    transformer = IncreasingWidthDiscretiser()
+    transformer = GeometricWidthDiscretiser()
     with pytest.raises(ValueError):
         transformer.fit(df_na)
 
 
 def test_error_if_input_df_contains_na_in_transform(df_vartypes, df_na):
     # test case 4: when dataset contains na, transform method
-    transformer = IncreasingWidthDiscretiser()
+    transformer = GeometricWidthDiscretiser()
     transformer.fit(df_vartypes)
     with pytest.raises(ValueError):
         transformer.transform(df_na[["Name", "City", "Age", "Marks", "dob"]])
 
 
 def test_non_fitted_error(df_vartypes):
-    transformer = IncreasingWidthDiscretiser()
+    transformer = GeometricWidthDiscretiser()
     with pytest.raises(NotFittedError):
         transformer.transform(df_vartypes)
