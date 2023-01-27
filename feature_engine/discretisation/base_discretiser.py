0fd3cc2838017f37459560ff8a6ea3e1369fae33
Gleb Levitski <36483986+glevv@users.noreply.github.com>
2023-01-27 17:43:20 +0000

    Update base_discretiser.py

--- feature_engine/discretisation/base_discretiser.py
+++ feature_engine/discretisation/base_discretiser.py
@@ -59,7 +59,7 @@ class BaseDiscretiser(BaseNumericalTransformer):
         if self.return_boundaries is True:
             for feature in self.variables_:
                 X[feature] = pd.cut(
-                    X[feature], self.binner_dict_[feature], precision=self._precision
+                    X[feature], self.binner_dict_[feature], precision=self.precision
                 )
             X[self.variables_] = X[self.variables_].astype(str)
 
