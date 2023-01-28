5e31d5954df62c9e29e3cbede9894fb93f3a94e4
Soledad Galli <solegalli@protonmail.com>
2023-01-28 20:17:23 -0300

    reword dictionary check (#602)
    * reword dictionary check
    
    * reword test name

--- feature_engine/_check_input_parameters/check_input_dictionary.py
+++ feature_engine/_check_input_parameters/check_input_dictionary.py
@@ -3,20 +3,20 @@ from typing import Optional
 
 def _check_numerical_dict(dict_: Optional[dict]) -> Optional[dict]:
     """
-    Checks if all values in dictionary are integers and floats. Can take None as
-    argument.
+    Checks that all values in the dictionary are integers and floats. It can take also
+    take None as value.
 
     Parameters
     ----------
-    the_dict : dict
-        The dictionary that will be checked
+    dict_ : dict
+        The dictionary that will be checked.
 
     Raises
     ------
     ValueError
-        If any of the values in the dictionary are not int or float
+        If any of the values in the dictionary are not int or float.
     TypeError
-        When argument type is not a dictionary.
+        When input type is not a dictionary.
     """
 
     if isinstance(dict_, dict):
