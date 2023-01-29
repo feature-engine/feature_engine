4dea90cb4aaee7d64cf33e784fe51a563242c1f6
Soledad Galli <solegalli@protonmail.com>
2023-01-29 11:16:17 -0300

    reword unseen, missing and ignore format (#609)
    * update unseen and ignore format docstrings
    
    * update missing values docstrings

--- feature_engine/_docstrings/init_parameters/all_trasnformers.py
+++ feature_engine/_docstrings/init_parameters/all_trasnformers.py
@@ -17,8 +17,8 @@ _drop_original_docstring = """drop_original: bool, default=False
     """.rstrip()
 
 _missing_values_docstring = """missing_values: string, default='raise'
-        Indicates if missing values should be ignored or raised. If 'raise' the
+        Indicates if missing values should be ignored or raised. If `'raise'` the
         transformer will return an error if the the datasets to `fit` or `transform`
-        contain missing values. If 'ignore', missing data will be ignored when learning
-        parameters or performing the transformation.
+        contain missing values. If `'ignore'`, missing data will be ignored when
+        learning parameters or performing the transformation.
         """.rstrip()
