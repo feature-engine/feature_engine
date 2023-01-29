e6435ed0bb23b1035c91800b81ee4418c4b4af58
Soledad Galli <solegalli@protonmail.com>
2023-01-28 22:16:15 -0300

    fix style errors

--- feature_engine/_docstrings/init_parameters/outliers.py
+++ feature_engine/_docstrings/init_parameters/outliers.py
@@ -1,6 +1,6 @@
 _capping_method_docstring = """capping_method: str, default='gaussian'
-        Desired outlier detection method. Can be 'gaussian', 'iqr', 'mad', 'quantiles'.
-    
+        Desired outlier detection method. Can be 'gaussian', 'iqr', 'mad',
+        'quantiles'. \n
         The transformer will find the maximum and / or minimum values beyond which a
         data point will be considered an outlier using:
         **'gaussian'**: the Gaussian approximation.
@@ -18,11 +18,9 @@ _fold_docstring = """fold: int or float, default=0.05 if `quantile`, or 3 otherw
         The factor used to multiply the std, MAD or IQR to calculate
         the maximum or minimum allowed values.
         Recommended values are 2 or 3 for the gaussian approximation,
-        1.5 or 3 for the IQR proximity rule and 3 or 3.5 for MAD rule.
-    
+        1.5 or 3 for the IQR proximity rule and 3 or 3.5 for MAD rule. \n
         If `capping_method='quantile'`, then `'fold'` indicates the percentile. So if
-        `fold=0.05`, the limits will be the 95th and 5th percentiles.
-    
+        `fold=0.05`, the limits will be the 95th and 5th percentiles. \n
         **Note**: Outliers will be removed up to a maximum of the 20th percentiles on
         both sides. Thus, when `capping_method='quantile'`, then `'fold'` takes values
         between 0 and 0.20.
