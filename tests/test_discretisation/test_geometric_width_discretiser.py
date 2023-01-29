db1111571225e92021978be5c80cb8e06f9bcacc
Soledad Galli <solegalli@protonmail.com>
2023-01-29 13:40:12 -0300

    reformat tests

--- tests/test_discretisation/test_geometric_width_discretiser.py
+++ tests/test_discretisation/test_geometric_width_discretiser.py
@@ -6,18 +6,41 @@ from sklearn.exceptions import NotFittedError
 from feature_engine.discretisation import GeometricWidthDiscretiser
 
 
-def test_init_and_fit_params(df_normal_dist):
-    transformer = GeometricWidthDiscretiser(
-        bins=10, variables=None, return_object=False
+# test init params
+@pytest.mark.parametrize("param", [0.1, "hola", (True, False), {"a": True}, 2])
+def test_raises_error_when_return_object_not_bool(param):
+    with pytest.raises(ValueError):
+        GeometricWidthDiscretiser(return_object=param)
+
+
+@pytest.mark.parametrize("param", [0.1, "hola", (True, False), {"a": True}, 2])
+def test_raises_error_when_return_boundaries_not_bool(param):
+    with pytest.raises(ValueError):
+        GeometricWidthDiscretiser(return_boundaries=param)
+
+
+@pytest.mark.parametrize("param", [0.1, "hola", (True, False), {"a": True}])
+def test_raises_error_when_precision_not_int(param):
+    with pytest.raises(ValueError):
+        GeometricWidthDiscretiser(precision=param)
+
+
+@pytest.mark.parametrize("param", [0.1, "hola", (True, False), {"a": True}])
+def test_raises_error_when_bins_not_int(param):
+    with pytest.raises(ValueError):
+        GeometricWidthDiscretiser(bins=param)
+
+
+@pytest.mark.parametrize("params", [(False, 1), (True, 10)])
+def test_correct_param_assignment_at_init(params):
+    param1, param2 = params
+    t = GeometricWidthDiscretiser(
+        return_object=param1, return_boundaries=param1, precision=param2, bins=param2
     )
-    transformer.fit(df_normal_dist)
-    # init params
-    assert transformer.bins == 10
-    assert transformer.variables is None
-    assert transformer.return_object is False
-    # fit params
-    assert transformer.variables_ == ["var"]
-    assert transformer.n_features_in_ == 1
+    assert t.return_object is param1
+    assert t.return_boundaries is param1
+    assert t.precision == param2
+    assert t.bins == param2
 
 
 def test_fit_and_transform_methods(df_normal_dist):
@@ -42,23 +65,9 @@ def test_fit_and_transform_methods(df_normal_dist):
 
 
 def test_automatically_find_variables_and_return_as_object(df_normal_dist):
-    transformer = GeometricWidthDiscretiser(
-        bins=10, variables=None, return_object=True
-    )
+    transformer = GeometricWidthDiscretiser(bins=10, variables=None, return_object=True)
     X = transformer.fit_transform(df_normal_dist)
     assert X["var"].dtypes == "O"
-
-
-@pytest.mark.parametrize("bins", ["other", 0.5, [1]])
-def test_error_when_bins_not_number(bins):
-    with pytest.raises(ValueError):
-        GeometricWidthDiscretiser(bins=bins)
-
-
-@pytest.mark.parametrize("return_object", ["other", 0.5, [1]])
-def test_error_if_return_object_not_bool(return_object):
-    with pytest.raises(ValueError):
-        GeometricWidthDiscretiser(return_object=return_object)
 
 
 def test_error_if_input_df_contains_na_in_fit(df_na):
