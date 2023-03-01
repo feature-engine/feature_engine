543f498f55d2299735957d6ec15d733242a25769
Morgan-Sell <morganpsell@gmail.com>
2023-03-01 14:57:54 -0500

    add class code base

--- feature_engine/selection/probe_feature_selection.py
+++ feature_engine/selection/probe_feature_selection.py
@@ -105,3 +105,157 @@ class ProbeFeatureSelection(BaseSelector):
     {transform}
     """
 
+    def __init__(
+        self,
+        estimator,
+        scoring: str = "roc_auc",
+        n_probes: int = 1,
+        distribution: str = "normal",
+        cv: int = 5,
+        random_state: int = 0,
+        confirm_variables: bool = False,
+    ):
+        if distribution not in ["normal", "binary", "uniform", "all"]:
+            raise ValueError(
+                "distribution takes on 'normal', 'binary', 'uniform', or 'all' as "
+                f"values. Got {distribution} instead."
+            )
+
+        if distribution == "all" and n_probes % 3 != 0:
+            raise ValueError(
+                "If distribution is 'all' the n_probes must be a multiple of 3. "
+                f"Got {n_probes} instead."
+            )
+
+        if not isinstance(n_probes, int):
+            raise ValueError(f"n_probes must be an integer. Got {n_probes} instead.")
+
+        super().__init__(confirm_variables)
+        self.estimator = estimator
+        self.scoring = scoring
+        self.distribution = distribution
+        self.cv = cv
+        self.n_probes = n_probes
+        self.random_state = random_state
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        """
+        Find the important features.
+
+        Parameters
+        ----------
+        X: pandas dataframe of shape = [n_samples, n_features]
+
+        y: array-like of shape (n_samples)
+                   Target variable. Required to train the estimator.
+                """
+        # check input dataframe
+        X, y = check_X_y(X, y)
+
+        # if required exclude variables that are not in the input dataframe
+        # instantiates the 'variables_' attribute
+        self._confirm_variables(X)
+
+        # find numerical variables
+        self.variables_ = find_or_check_numerical_variables(X, None)
+
+        # save input features
+        self._get_feature_names_in(X)
+
+        # create probe feature distributions
+        self.probe_features_ = self._generate_probe_features(X.shape[0])
+
+        X_new = pd.concat([X, self.probe_features_], axis=1)
+
+        # train model with all variables including the probe features
+        model = cross_validate(
+            self.estimator,
+            X_new,
+            y,
+            cv=self.cv,
+            scoring=self.scoring,
+            return_estimator=True,
+        )
+
+        # Initialize a dataframe that will contain the list of the feature/coeff
+        # importance for each cross-validation fold
+        feature_importances_cv = pd.DataFrame()
+
+        # Populate the feature_importances_cv dataframe with columns containing
+        # the feature importance values for each model returned by the cross
+        # validation.
+        # There are as many columns as folds.
+        for i in range(len(model["estimator"])):
+            m = model["estimator"][i]
+            feature_importances_cv[i] = get_feature_importances(m)
+
+        # add the variables as the index to feature_importances_cv
+        feature_importances_cv.index = X_new.columns
+
+        # aggregate the feature importance returned in each fold
+        self.feature_importances_ = feature_importances_cv.mean(axis=1)
+
+        # get features that have an importance less than the probe features'
+        # avg importance attribute used in transform() which is inherited
+        # from parent class
+        self.features_to_drop_ = self._get_features_to_drop()
+
+        return self
+
+    def _generate_probe_features(self, n_obs: int) -> pd.DataFrame:
+        """
+        Returns a dataframe comprised of the probe feature using the
+        selected distribution.
+        """
+        # create dataframe
+        df = pd.DataFrame()
+
+        # set random state
+        np.random.seed(self.random_state)
+        if self.distribution == "all":
+            generation_cnt = self.n_probes // 3
+            for i in range(generation_cnt):
+                df[f"gaussian_probe_{i}"] = np.random.normal(0, 3, n_obs)
+                df[f"binary_probe_{i}"] = np.random.randint(0, 2, n_obs)
+                df[f"uniform_probe_{i}"] = np.random.uniform(0, 1, 1).tolist() * n_obs
+
+        # when distribution is normal, binary, or uniform
+        else:
+            for i in range(self.n_probes):
+                if self.distribution == "normal":
+                    df[f"gaussian_probe_{i}"] = np.random.normal(0, 3, n_obs)
+
+                elif self.distribution == "binary":
+                    df[f"binary_probe_{i}"] = np.random.randint(0, 2, n_obs)
+
+                elif self.distribution == "uniform":
+                    df[f"uniform_probe_{i}"] = (
+                            np.random.uniform(0, 1, 1).tolist() * n_obs
+                    )
+
+        return df
+
+    def _get_features_to_drop(self):
+        """
+        Identify the variables that have a lower feature importance than the average
+        feature importance of all the probe features.
+        """
+
+        # if more than 1 probe feature, calculate average feature importance
+        if self.n_probes > 1:
+            probe_features_avg_importance = self.feature_importances_[
+                self.probe_features_.columns
+            ].values.mean()
+
+        else:
+            probe_features_avg_importance = self.feature_importances_[
+                self.probe_features_.columns
+            ].values
+
+        features_to_drop = []
+
+        for var in self.variables_:
+            if self.feature_importances_[var] < probe_features_avg_importance:
+                features_to_drop.append(var)
+
+        return features_to_drop
