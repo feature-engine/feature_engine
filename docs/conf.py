c3334ffef69b9d3d25689d680f9fd47125c4ce8a
Soledad Galli <solegalli@protonmail.com>
2023-03-07 14:11:04 +0100

    update favicon usage

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     "sphinx_autodoc_typehints",  # Automatically document param types (less noise in class signature)
     "numpydoc",
     "sphinx.ext.linkcode",
+    "sphinx_favicon",
     # "sphinx.ext.doctest",
 ]
 
@@ -157,7 +158,7 @@ html_logo = "images/logo/Logo_name.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "images/logo/favicon.png"
+favicons = ["favicon.png"]
 
 # Custom style
 html_style = "css/feature-engine.css"
