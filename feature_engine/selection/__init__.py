d3c4634e05165b2cdda68864c90e383921c65c3e
Soledad Galli <solegalli@protonmail.com>
2023-03-14 10:09:45 +0100

    tidy init

--- feature_engine/selection/__init__.py
+++ feature_engine/selection/__init__.py
@@ -7,13 +7,13 @@ from .drop_duplicate_features import DropDuplicateFeatures
 from .drop_features import DropFeatures
 from .drop_psi_features import DropHighPSIFeatures
 from .information_value import SelectByInformationValue
+from .probe_feature_selection import ProbeFeatureSelection
 from .recursive_feature_addition import RecursiveFeatureAddition
 from .recursive_feature_elimination import RecursiveFeatureElimination
 from .shuffle_features import SelectByShuffling
 from .single_feature_performance import SelectBySingleFeaturePerformance
 from .smart_correlation_selection import SmartCorrelatedSelection
 from .target_mean_selection import SelectByTargetMeanPerformance
-from .probe_feature_selection import ProbeFeatureSelection
 
 __all__ = [
     "DropFeatures",
