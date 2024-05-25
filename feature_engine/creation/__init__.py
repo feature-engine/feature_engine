78d090b7d68a1b6d3e831fc59f963686bdc1a46d
Soledad Galli <solegalli@protonmail.com>
2024-05-25 17:11:42 +0200

    sort init

--- feature_engine/creation/__init__.py
+++ feature_engine/creation/__init__.py
@@ -2,11 +2,10 @@
 The module creation includes classes to create new variables by combination of existing
 variables in the dataframe.
 """
-# FIXME: remove in version 1.4
 from .cyclical_features import CyclicalFeatures
+from .decision_tree_features import DecisionTreeFeatures
 from .math_features import MathFeatures
 from .relative_features import RelativeFeatures
-from .decision_tree_features import DecisionTreeFeatures
 
 __all__ = [
     "DecisionTreeFeatures",
