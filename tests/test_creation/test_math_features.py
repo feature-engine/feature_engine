4886f0abe20d1e48e8fe504bd2aba9d1c9764cde
olikra <olikra68@gmail.com>
2024-06-12 08:49:45 +0200

    Initial commit to test the GitHub workflow to fork and main. Code was reformatted on the import statements and a new blank line between linenumber 73 and 74

--- tests/test_creation/test_math_features.py
+++ tests/test_creation/test_math_features.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
 import pytest
-from sklearn.pipeline import Pipeline
-
 from feature_engine.creation import MathFeatures
+from sklearn.pipeline import Pipeline
 
 
 # test param variables_to_combine
@@ -71,6 +70,7 @@ def test_error_new_variable_names_not_permitted():
             func="sum",
             new_variables_names=["sum_of_two_vars", "mean_of_two_vars"],
         )
+
     with pytest.raises(ValueError):
         MathFeatures(
             variables=variables,
