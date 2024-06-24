5310466276b58756fe9a51028cb2111783627715
olikra <olikra68@gmail.com>
2024-06-24 08:44:17 +0200

    add doc for customfunction

--- feature_engine/creation/math_features.py
+++ feature_engine/creation/math_features.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional, Union
 
+import numpy as np
 import pandas as pd
-
 from feature_engine._docstrings.fit_attributes import (
     _feature_names_in_docstring,
     _n_features_in_docstring,
@@ -130,6 +130,58 @@ class MathFeatures(BaseCreation):
     0   1   4         2.5
     1   2   5         3.5
     2   3   6         4.5
+
+    Examples for Custom Functions
+    -----------------------------
+    We do not recommend using a custum function in combination with
+    the pandas.agg(). Due to performance issues it should be avoided.
+    For the sake of completeness a short example:
+
+    >>> import pandas as pd
+    >>> from feature_engine.creation import MathFeatures
+    >>>
+    >>> def customfunction_agg(series):
+    >>>    # pandas.agg calls the custom-function twice
+    >>>    # first with a non series type
+    >>>    # second with a series type -> we need the series type
+    >>>    if not isinstance(series, pd.Series):
+    >>>        raise ValueError("Only Series allowed")
+    >>>    result = series["Age"] + series["Marks"]
+    >>>    return result
+
+    >>> X = pd.DataFrame(dict(x1 = [1,2,3], x2 = [4,5,6]))
+    >>> mf = MathFeatures(variables = ["x1","x2"], func = "customfunction_agg")
+    >>> mf.fit(X)
+    >>> X = mf.transform(X))
+       x1  x2  customfunction_agg_x1_x2
+    0   1   4         5
+    1   2   5         7
+    2   3   6         9
+
+    We recommend the usage of custom functions via the numpy.apply_over_axes().
+    A custom function gets processed via numpy.apply_over_axes() when we extend
+    a provided wrapper class.
+
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> from feature_engine.creation import MathFeatures
+    >>> from feature_engine.creation.custom_functions import CustomFunctions
+    >>> class custom_function_1(CustomFunctions):
+    >>>    def domain_specific_custom_function_1(self, df, a):
+    >>>       result = np.sum(df, axis=1)
+    >>>       return result
+    >>> cufu = custom_function_1(scope_target="numpy")
+    >>> X = pd.DataFrame(dict(x1 = [1,2,3], x2 = [4,5,6]))
+    >>> mf = MathFeatures(variables = ["x1","x2"], func = [cufu.domain_specific_custom_function_1])
+    >>> mf.fit(X)
+    >>> X = mf.transform(X)
+
+        x1  x2        domain_specific_custom_function_1_x1_x2
+    0   1   4         5
+    1   2   5         7
+    2   3   6         9
+
+
     """
 
     def __init__(
@@ -140,6 +192,16 @@ class MathFeatures(BaseCreation):
         missing_values: str = "raise",
         drop_original: bool = False,
     ) -> None:
+
+        # casting input parameter func to a list
+        function_input = []
+        if isinstance(func, str):
+            function_input.append(func)
+        elif isinstance(func, list):
+            function_input = func
+        else:
+            function_input = func
+        func = function_input
 
         if (
             not isinstance(variables, list)
@@ -202,14 +264,109 @@ class MathFeatures(BaseCreation):
         X_new: Pandas dataframe, shape = [n_samples, n_features + n_operations]
             The input dataframe plus the new variables.
         """
+
+        def np_transform(np_df, new_variable_names, np_variables, np_functions):
+            #    calculations = ('mean','sum', 'min', 'max', 'prod', 'median', 'std', 'var')
+            np_result_df = df = pd.DataFrame()
+            for np_function_idx, np_function in enumerate(np_functions):
+                if np_function in ("sum", "np.sum"):
+                    result = np.nansum(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+                    pass
+
+                elif np_function in  ("mean", "np.mean"):
+                    result = np.nanmean(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("min", "np.min"):
+                    result = np.nanmin(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("max", "np.max"):
+                    result = np.nanmax(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("prod", "np.prod"):
+                    result = np.nanprod(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("median", "np.median"):
+                    result = np.nanmedian(
+                        np_df[np_variables],
+                        axis=1,
+                    )
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("std", "np.std"):
+                    result = np.nanstd(np_df[np_variables], axis=1, ddof=1)
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                elif np_function in ("var", "np.var"):
+                    result = np.nanvar(np_df[np_variables], axis=1, ddof=1)
+                    np_result_df[new_variable_names[np_function_idx]] = pd.Series(
+                        result
+                    )
+
+                else:
+                    try:
+                        scope_target = np_function.__self__.scope_target
+                    except:
+                        scope_target = "pandas"
+
+                    if scope_target == "numpy":
+                        result = np.apply_over_axes(np_function, np_df[np_variables], 1)
+                        np_result_df[new_variable_names[np_function_idx]] = (
+                            pd.DataFrame(
+                                data=result,
+                                columns=[new_variable_names[np_function_idx]],
+                            )
+                        )
+                    elif scope_target == "pandas":
+                        result = np_df[np_variables].agg(np_function, axis=1)
+                        np_result_df[new_variable_names[np_function_idx]] = result
+
+                    print(scope_target)
+
+            return np_result_df
+
         X = self._check_transform_input_and_state(X)
 
         new_variable_names = self._get_new_features_name()
 
-        if len(new_variable_names) == 1:
-            X[new_variable_names[0]] = X[self.variables].agg(self.func, axis=1)
-        else:
-            X[new_variable_names] = X[self.variables].agg(self.func, axis=1)
+
+        X = pd.concat(
+            [X, np_transform(X, new_variable_names, self.variables, self.func)],
+            axis=1,
+        )
 
         if self.drop_original:
             X.drop(columns=self.variables, inplace=True)
