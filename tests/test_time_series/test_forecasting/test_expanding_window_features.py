3594192726825df9a981ed26bd22e222ca89ff3c
olikra <olikra68@gmail.com>
2024-07-04 14:18:54 +0200

    isort

--- tests/test_time_series/test_forecasting/test_expanding_window_features.py
+++ tests/test_time_series/test_forecasting/test_expanding_window_features.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import pytest
-from feature_engine.timeseries.forecasting import ExpandingWindowFeatures
 from pandas.testing import assert_frame_equal
+
+from feature_engine.timeseries.forecasting import ExpandingWindowFeatures
 
 
 def test_get_feature_names_out_raises_when_input_features_is_string(df_time):
