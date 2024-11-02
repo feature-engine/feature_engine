d6b04ef56c0b0758671cf7c76c0dbb77a0bf18f4
solegalli <solegalli@protonmail.com>
2024-11-02 20:13:22 +0100

    add new code bit for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -279,6 +279,17 @@ linkcode_resolve = make_linkcode_resolve(
     "{package}/{path}#L{lineno}",
 )
 
+# -- Based on new functionality in readthedocs ----------------------------------------
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 
 def setup(app):
     # plausible analytics
