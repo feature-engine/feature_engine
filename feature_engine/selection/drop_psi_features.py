fb004034406f2286a918383917ee35d1a7099106
solegalli <solegalli@protonmail.com>
2025-07-05 13:46:52 -0400

    reworded docs to reflect symmetric psi

--- feature_engine/selection/drop_psi_features.py
+++ feature_engine/selection/drop_psi_features.py
@@ -171,8 +171,9 @@ class DropHighPSIFeatures(BaseSelector):
 
     switch: boolean, default=False.
         If True, the order of the 2 dataframes used to determine the PSI (basis and
-        test) will be switched. This is important because the PSI is not symmetric,
-        i.e., PSI(a, b) != PSI(b, a)).
+        test) will be switched. This is important because the interval limits used to
+        calculate the PSI are inferred from the basis dataframe. Hence, changing the
+        order of the dataframes may lead to different PSI values.
 
     threshold: float, str, default = 0.25.
         The threshold to drop a feature. If the PSI for a feature is >= threshold, the
