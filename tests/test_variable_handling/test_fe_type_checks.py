9d443033a03899f8df130cf2237f9e0ee1d792d0
solegalli <solegalli@protonmail.com>
2026-01-27 22:16:41 -0500

    expand tests

--- tests/test_variable_handling/test_fe_type_checks.py
+++ tests/test_variable_handling/test_fe_type_checks.py
@@ -1,10 +1,18 @@
 from feature_engine.variable_handling._variable_type_checks import (
     _is_categorical_and_is_datetime,
     _is_categorical_and_is_not_datetime,
+    _is_categories_num,
     _is_convertible_to_dt,
     _is_convertible_to_num,
-    _is_categories_num,
 )
+
+
+def test_is_categories_num(df):
+    assert _is_categories_num(df["Name"]) is False
+
+    df["Age"] = df["Age"].astype("category")
+    assert _is_categories_num(df["Age"]) is True
+
 
 def test_is_convertible_to_num(df):
     assert _is_convertible_to_num(df["Name"]) is False
@@ -30,6 +38,7 @@ def test_is_categorical_and_is_datetime(df, df_datetime):
 
     df["age_str"] = ["20", "21", "19", "18"]
     assert _is_categorical_and_is_datetime(df["age_str"]) is False
+
 
 def test_is_categorical_and_is_not_datetime(df):
     assert _is_categorical_and_is_not_datetime(df["date_obj0"]) is False
