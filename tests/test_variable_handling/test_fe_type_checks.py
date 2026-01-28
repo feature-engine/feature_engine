6ff27aa30a115ae7354dcef2b692a1c72b0313a1
solegalli <solegalli@protonmail.com>
2026-01-27 21:59:00 -0500

    expand tests

--- tests/test_variable_handling/test_fe_type_checks.py
+++ tests/test_variable_handling/test_fe_type_checks.py
@@ -1,26 +1,40 @@
-import pytest
-
 from feature_engine.variable_handling._variable_type_checks import (
     _is_categorical_and_is_datetime,
     _is_categorical_and_is_not_datetime,
     _is_convertible_to_dt,
-
+    _is_convertible_to_num,
+    _is_categories_num,
 )
 
 def test_is_convertible_to_num(df):
-    assert _is_convertible_to_dt(df["Name"]) is False
-    assert _is_convertible_to_dt(df["date_obj0"]) is True
+    assert _is_convertible_to_num(df["Name"]) is False
+    assert _is_convertible_to_num(df["date_obj0"]) is False
+
+    df["age_str"] = ["20", "21", "19", "18"]
+    assert _is_convertible_to_num(df["age_str"]) is True
+
 
 def test_is_convertible_to_dt(df):
     assert _is_convertible_to_dt(df["date_obj0"]) is True
     assert _is_convertible_to_dt(df["date_range"]) is True
     assert _is_convertible_to_dt(df["Name"]) is False
 
-def test_is_categorical_and_is_datetime(df):
+    df["age_str"] = ["20", "21", "19", "18"]
+    assert _is_convertible_to_dt(df["age_str"]) is False
+
+
+def test_is_categorical_and_is_datetime(df, df_datetime):
     assert _is_categorical_and_is_datetime(df["date_obj0"]) is True
     assert _is_categorical_and_is_datetime(df["Name"]) is False
+    assert _is_categorical_and_is_datetime(df_datetime["date_obj1"]) is True
+
+    df["age_str"] = ["20", "21", "19", "18"]
+    assert _is_categorical_and_is_datetime(df["age_str"]) is False
 
 def test_is_categorical_and_is_not_datetime(df):
     assert _is_categorical_and_is_not_datetime(df["date_obj0"]) is False
     assert _is_categorical_and_is_not_datetime(df["date_obj0"]) is False
     assert _is_categorical_and_is_not_datetime(df["Name"]) is True
+
+    df["age_str"] = ["20", "21", "19", "18"]
+    assert _is_categorical_and_is_not_datetime(df["age_str"]) is True
