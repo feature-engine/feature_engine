b713e01f46145761cc499093c1fe27f21ff7cf1e
Aniq Ramzan <aniqramzan5758@gmail.com>
2026-07-26 13:27:53 +0500

    style: fix flake8 formatting issues

--- feature_engine/imputation/__init__.py
+++ feature_engine/imputation/__init__.py
@@ -2,24 +2,23 @@
 The module imputation includes classes to perform missing data imputation
 """
 
-from .arbitrary_number import ArbitraryNumberImputer, ArbitraryImputer
+from .arbitrary_number import ArbitraryImputer, ArbitraryNumberImputer
 from .categorical import CategoricalImputer
 from .drop_missing_data import DropMissingData
 from .end_tail import EndTailImputer
-from .mean_median import MeanMedianImputer, MeanImputer
+from .mean_median import MeanImputer, MeanMedianImputer
 from .missing_indicator import AddMissingIndicator, MissingIndicator
 from .random_sample import RandomSampleImputer
 
 __all__ = [
-    "MeanMedianImputer",      # deprecated
-    "ArbitraryNumberImputer", # deprecated
-    "AddMissingIndicator",    # deprecated
     "MeanImputer",
-    "ArbitaryImputer",
+    "MeanMedianImputer",
+    "ArbitraryImputer",
+    "ArbitraryNumberImputer",
+    "MissingIndicator",
+    "AddMissingIndicator",
     "CategoricalImputer",
     "EndTailImputer",
-    "MissingIndicator",
     "RandomSampleImputer",
     "DropMissingData",
 ]
-
